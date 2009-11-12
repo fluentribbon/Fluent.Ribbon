30483f8d355b07c8793d17e4658bf58e951a58b2
SableRaven <none@none>
2009-11-12 10:57:28 +0000

    Adding some non-working functionality.

---
+++ Fluent/Fluent/RibbonControl.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Fluent
+{
+    [TemplatePart(Name = "PART_RibbonPanel", Type = typeof(RibbonPanel))]
+    [ContentProperty("Tabs")]
+    public class RibbonControl: FrameworkElement, IAddChild
+    {
+        #region Статический атрибуты
+
+        // Дефолтный стиль для окна
+        private static readonly Style DefaultWindowStyle = null;
+        // Дефолтный словарь с темами
+        private static readonly ResourceDictionary DefaultResourceDictionary = null;
+
+        #endregion
+
+        #region Атрибуты
+
+        private UIElementCollection tabs;
+
+        private RibbonPanel panel;
+
+        #endregion
+
+        #region Свойства
+
+        public UIElementCollection Tabs
+        {
+            get
+            {
+                return tabs;
+            }
+        }
+
+        #endregion
+
+        #region Инициализация
+
+        static RibbonControl()
+        {
+            StyleProperty.OverrideMetadata(typeof(RibbonControl), new FrameworkPropertyMetadata(null, new CoerceValueCallback(OnCoerceStyle)));
+            /*DefaultStyleKeyProperty.OverrideMetadata(typeof(RibbonControl), new FrameworkPropertyMetadata(typeof(RibbonControl)));
+
+            DefaultResourceDictionary = new ResourceDictionary();
+            DefaultResourceDictionary.BeginInit();
+            DefaultResourceDictionary.Source = new Uri("Fluent;component/Styles/DefaultSkinWindowStyle.xaml", UriKind.Relative);
+            DefaultResourceDictionary.EndInit();
+            DefaultWindowStyle = (Style)DefaultResourceDictionary["DefaultSkinWindowStyle"];*/
+        }
+
+        /// <summary>
+        /// При необходимости устанавливается ныжный стиль
+        /// </summary>
+        /// <param name="o">Не используется</param>
+        /// <param name="value">Значение из словаря</param>
+        /// <returns>Правильный словарь</returns>
+        private static object OnCoerceStyle(DependencyObject o, object value)
+        {
+            if (value == null) value = RibbonControl.DefaultWindowStyle;
+            return value;
+        }
+
+        /// <summary>
+        /// При применении нового стиля
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            panel = FindName("PART_RibbonPanel") as RibbonPanel;
+            if(panel == null) throw new Exception("Incorrect control template.");
+        }
+
+        public RibbonControl()
+        {
+            tabs = new UIElementCollection(this, this);
+        }
+
+        #endregion
+
+        #region Реализация IAddChild
+
+        public void AddChild(object value)
+        {
+            Tabs.Add(value as UIElement);
+        }
+
+        public void AddText(string text)
+        {
+            TextBlock textBox = new TextBlock();
+            textBox.Text = text;
+            Tabs.Add(textBox);
+        }
+
+        #endregion
+    }
+}
---
+++ Fluent/Fluent/RibbonTab.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Fluent
+{
+    internal enum RibbonTabState
+    {
+        Unselected = 0,
+        Selected,
+        SelectedForPopup,
+        SelectedForKeyTip
+    }
+
+    public class RibbonTab:ContentControl
+    {
+        #region Attributes
+        
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get os set tab header
+        /// </summary>
+        public object Header
+        {
+            get { return (object)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        /// <summary>
+        /// Get os set tab header
+        /// </summary>
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(object), typeof(RibbonTab), new UIPropertyMetadata(""));
+
+        /// <summary>
+        /// Get or set tab state
+        /// </summary>
+        internal RibbonTabState TabState
+        {
+            get { return (RibbonTabState)GetValue(TabStateProperty); }
+            set { SetValue(TabStateProperty, value); }
+        }
+
+        /// <summary>
+        /// Get or set tab state
+        /// </summary>
+        internal static readonly DependencyProperty TabStateProperty = DependencyProperty.Register("TabState", typeof(RibbonTabState), typeof(RibbonTab), new FrameworkPropertyMetadata(RibbonTabState.Unselected, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnTabStateChanged));
+
+        #endregion
+
+        #region Events
+
+        public event RoutedEventHandler TabChanged;
+
+        #endregion
+
+        #region Initialize
+
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static RibbonTab()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RibbonTab), new FrameworkPropertyMetadata(typeof(RibbonTab)));
+        }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public RibbonTab()
+        {
+            
+        }
+
+        #endregion
+
+        #region Event handling
+
+        private static void OnTabStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            RibbonTab tab = d as RibbonTab;
+            if (tab.TabChanged != null) tab.TabChanged(tab, (RoutedEventArgs)EventArgs.Empty);
+        }
+
+        #endregion
+    }
+}
