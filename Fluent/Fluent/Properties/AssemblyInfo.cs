8c9bf265027aded2c56bfca7b72c394a292f45fd
SableRaven <none@none>
2009-11-23 09:48:54 +0000

    Added RibbonContextualTabGroup.

--- Fluent/Fluent/Properties/AssemblyInfo.cs
+++ Fluent/Fluent/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
---
+++ Fluent/Fluent/RibbonContextualTabGroup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Fluent
+{
+    [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(RibbonTabItem))]    
+    [TemplatePart(Name = "PART_BorderTop", Type = typeof(Border))]
+    [TemplatePart(Name = "PART_BorderBottom", Type = typeof(Border))]
+    public class RibbonContextualTabGroup : HeaderedItemsControl
+    {
+        #region Fields
+
+        private Border borderTop = null;
+        private Border borderBottom = null;
+
+        #endregion
+
+        #region Initialize
+
+        static RibbonContextualTabGroup()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RibbonContextualTabGroup), new FrameworkPropertyMetadata(typeof(RibbonContextualTabGroup)));
+        }
+
+        public RibbonContextualTabGroup()
+        {
+            
+        }
+
+        #endregion
+
+        #region Overrides
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new RibbonTabItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return (item is RibbonTabItem);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            borderTop = GetTemplateChild("PART_BorderTop") as Border;
+            borderBottom = GetTemplateChild("PART_BorderBottom") as Border;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            if ((borderTop == null) || (borderBottom == null)) return base.MeasureOverride(constraint);
+            Size infinity = new Size(double.PositiveInfinity, double.PositiveInfinity);
+            borderTop.Measure(infinity);
+            borderBottom.Measure(infinity);
+            double widthTop = borderTop.DesiredSize.Width;
+            double widthBottom = borderBottom.DesiredSize.Width;
+            double width = Math.Min(Math.Max(widthTop, widthBottom),constraint.Width);
+            //TODO: need to conside width of caption buttons for top border
+            borderTop.Measure(new Size(width,double.PositiveInfinity));
+            borderBottom.Measure(new Size(width, double.PositiveInfinity));
+            return new Size(width, borderTop.DesiredSize.Height + borderBottom.DesiredSize.Height);
+        }
+
+        #endregion
+    }
+}
