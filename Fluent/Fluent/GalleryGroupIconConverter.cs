d9331c5509231165014d815d662143d97f544974
SableRaven <none@none>
2010-02-16 11:15:29 +0000

    Fixed bug with gallery group icon.

--- Fluent/Fluent/GalleryGroupIconConverter.cs
+++ Fluent/Fluent/GalleryGroupIconConverter.cs
@@ -6,12 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Fluent
 {
-    public class GalleryGroupIconConverter : IValueConverter
+    public class GalleryGroupIconConverter : IMultiValueConverter
     {
-        #region Implementation of IValueConverter
+        /*#region Implementation of IValueConverter
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -26,6 +27,27 @@ namespace Fluent
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion*/
+
+        #region Implementation of IMultiValueConverter
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Gallery gallery = VisualTreeHelper.GetParent(values[1] as DependencyObject) as Gallery;
+            if (gallery != null)
+            {
+                string group = gallery.GetItemGroupName((values[0] as ReadOnlyObservableCollection<object>)[0]);
+                if (gallery.GroupIcons.Count(x => x.GroupName == group) == 0) return null;
+                return gallery.GroupIcons.First(x => x.GroupName == group).Icon;
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
