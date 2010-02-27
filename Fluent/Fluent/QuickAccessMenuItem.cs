237eae09e41a4deee58e3d8d7ff2130cae54bcc3
daVinci <none@none>
2010-02-27 06:52:51 +0000

    Group now can be added to QAT when it is open

--- Fluent/Fluent/QuickAccessMenuItem.cs
+++ Fluent/Fluent/QuickAccessMenuItem.cs
@@ -71,7 +71,7 @@ namespace Fluent
         #region Target Property
 
         /// <summary>
-        /// Gets or sets shortcut control
+        /// Gets or sets shortcut to the target control
         /// </summary>
         public Control Target
         {
@@ -134,7 +134,7 @@ namespace Fluent
 
         #region Event Handlers
 
-        private void OnChecked(object sender, RoutedEventArgs e)
+        void OnChecked(object sender, RoutedEventArgs e)
         {
             Ribbon ribbon = FindRibbon();
             if (ribbon != null)
@@ -143,7 +143,7 @@ namespace Fluent
             }
         }
 
-        private void OnUnchecked(object sender, RoutedEventArgs e)
+        void OnUnchecked(object sender, RoutedEventArgs e)
         {
             Ribbon ribbon = FindRibbon();
             if (ribbon != null)
@@ -152,7 +152,7 @@ namespace Fluent
             }
         }
 
-        private void OnItemLoaded(object sender, RoutedEventArgs e)
+        void OnItemLoaded(object sender, RoutedEventArgs e)
         {
             Ribbon ribbon = FindRibbon();
             if (ribbon != null)
@@ -204,12 +204,7 @@ namespace Fluent
         {
             IQuickAccessItemProvider provider = (element as IQuickAccessItemProvider);
             if ((provider != null) && (provider.CanAddToQuickAccessToolBar)) return true;
-            /*if ((element is Button) ||
-                (element is CheckBox) ||
-                (element is RadioButton) ||
-                (element is ComboBox) ||
-                (element is TextBox)) return true;
-            else */return false;
+            return false;
         }
 
         /// <summary>
@@ -224,14 +219,9 @@ namespace Fluent
 
             // If control supports the interface just return what it provides 
             IQuickAccessItemProvider provider = (element as IQuickAccessItemProvider);
-            if ((provider!=null)&&(provider.CanAddToQuickAccessToolBar)) result = (element as IQuickAccessItemProvider).CreateQuickAccessItem();
-
-            /*// Predefined controls            
-            else if (element is TextBox) result = GetTextBoxQuickAccessItem(element as TextBox);
-            else if (element is ComboBox) result = GetComboBoxQuickAccessItem(element as ComboBox);
-            else if (element is ToggleButton) result = GetToggleButtonQuickAccessItem(element as ToggleButton);
-            else if (element is Button) result = GetButtonQuickAccessItem((Button)element);            
-            */
+            if ((provider != null) && (provider.CanAddToQuickAccessToolBar)) 
+                result = ((IQuickAccessItemProvider)element).CreateQuickAccessItem();
+            
             // The control isn't supported
             if (result == null) throw new ArgumentException("The contol " + element.GetType().Name + " is not able to provide a quick access toolbar item");
             
@@ -261,199 +251,18 @@ namespace Fluent
         {
             HitTestResult result = VisualTreeHelper.HitTest(visual, point);
             if (result == null) return null;
+            
+            // Try to find in visual (or logical) tree
             UIElement element = result.VisualHit as UIElement;
             while (element != null)
             {
                 if(IsSupported(element)) return element;
-                element = VisualTreeHelper.GetParent(element) as UIElement;
+                UIElement visualParent = VisualTreeHelper.GetParent(element) as UIElement;
+                UIElement logicalParent = LogicalTreeHelper.GetParent(element) as UIElement;
+                element = visualParent ?? logicalParent;
             }
+
             return null;
-        }
-
-        #endregion
-
-        #region Buttons
-
-        static UIElement GetButtonQuickAccessItem(Button button)
-        {
-           
-
-            Button item = new Button();
-            item.Focusable = false;
-
-
-            // Copy common properties
-            BindControlProperties(button, item);
-            // Bind small icon
-            if (button.Icon != null)
-                Bind(button, item, "Icon",RibbonControl.IconProperty, BindingMode.OneWay);
-            if (button.LargeIcon != null)
-                Bind(button, item, "LargeIcon", Button.LargeIconProperty, BindingMode.OneWay);
-
-            // TODO: check, maybe copy style is not required for quick access toolbar items
-            Bind(button, item, "Style", Button.StyleProperty, BindingMode.OneWay);
-            item.Size = RibbonControlSize.Small;
-
-            
-            // Syncronization
-            item.Click += delegate(object sender, RoutedEventArgs e) { button.RaiseEvent(e); };
-
-            
-            return item;
-        }
-
-        #endregion
-
-        #region CheckBoxes & RadioButtons (ToggleButtons)
-
-        
-        static UIElement GetToggleButtonQuickAccessItem(ToggleButton toggleButton)
-        {
-           
-
-            ToggleButton item = new ToggleButton();
-            item.Focusable = false;
-            //item.Content = (toggleButton.Content != null) ? toggleButton.Content.ToString() : null;
-            item.Text = toggleButton.Text;
-            item.Size = RibbonControlSize.Small;
-            
-            // Copy common properties
-            BindControlProperties(toggleButton, item);
-
-            // Copy small icon
-            if (toggleButton.Icon != null) 
-                Bind(toggleButton, item, "Icon", 
-                    RibbonControl.IconProperty, BindingMode.OneWay);
-            
-            // TODO: check, maybe copy style is not required for quick access toolbar items
-            Bind(toggleButton, item, "Style", ToggleButton.StyleProperty, BindingMode.OneWay);
-
-            // Syncronization            
-            Bind(toggleButton, item, "IsChecked", ToggleButton.IsCheckedProperty, BindingMode.TwoWay);
-
-            return item;
-        }
-
-        #endregion
-
-        #region ComboBoxes
-
-        static UIElement GetComboBoxQuickAccessItem(ComboBox comboBox)
-        {
-
-
-            ComboBox item = new ComboBox();
-            item.Focusable = false;
-            
-
-            // Copy common properties
-            BindControlProperties(comboBox, item);
-            
-            // Copy small icon
-            /*if (RibbonControl.GetSmallIcon(comboBox) != null)
-                Bind(comboBox, item, "(Fluent:RibbonControl.SmallIcon)",
-                    RibbonControl.SmallIconProperty, BindingMode.OneWay);*/
-
-
-            Bind(comboBox, item, "Width", ComboBox.WidthProperty, BindingMode.OneWay);
-            Bind(comboBox, item, "Heigth", ComboBox.HeightProperty, BindingMode.OneWay);
-            /*Bind(comboBox, item, "MaxDropDownHeight", ComboBox.MaxDropDownHeightProperty, BindingMode.OneWay);
-            Bind(comboBox, item, "StaysOpenOnEdit", ComboBox.StaysOpenOnEditProperty, BindingMode.OneWay);
-            
-
-            Bind(comboBox, item, "IsEditable", ComboBox.IsEditableProperty, BindingMode.OneWay);
-            Bind(comboBox, item, "IsReadOnly", ComboBox.IsReadOnlyProperty, BindingMode.TwoWay);
-            Bind(comboBox, item, "Text", ComboBox.TextProperty, BindingMode.TwoWay);
-
-            item.ItemsSource = comboBox.Items;            
-            Bind(comboBox, item, "Items", ComboBox.ItemsSourceProperty, BindingMode.OneWay);
-            */
-            return item;
-        }
-
-        #endregion
-
-        #region TextBoxes
-                
-        static UIElement GetTextBoxQuickAccessItem(TextBox textBox)
-        {
-           
-
-            TextBox item = new TextBox();
-            //item.Focusable = false;
-            Bind(textBox, item, "Width", Control.WidthProperty, BindingMode.OneWay);
-            Bind(textBox, item, "Heigth", Control.HeightProperty, BindingMode.OneWay);
-
-            // Copy common properties
-            BindControlProperties(textBox, item);
-            
-            // Copy small icon
-            /*if (RibbonControl.GetSmallIcon(textBox) != null)
-                Bind(textBox, item, "(Fluent:RibbonControl.SmallIcon)",
-                    RibbonControl.SmallIconProperty, BindingMode.OneWay);*/
-                        
-            Bind(textBox, item, "IsReadOnly", TextBox.IsReadOnlyProperty, BindingMode.OneWay);
-            //Bind(textBox, item, "Text", TextBox.TextProperty, BindingMode.OneWay);
-            Bind(textBox, item, "CharacterCasing", TextBox.CharacterCasingProperty, BindingMode.OneWay);
-            Bind(textBox, item, "MaxLength", TextBox.MaxLengthProperty, BindingMode.OneWay);
-            Bind(textBox, item, "MaxLines", TextBox.MaxLinesProperty, BindingMode.OneWay);
-            Bind(textBox, item, "MinLines", TextBox.MinLinesProperty, BindingMode.OneWay);
-            Bind(textBox, item, "TextAlignment", TextBox.TextAlignmentProperty, BindingMode.OneWay);
-            Bind(textBox, item, "TextDecorations", TextBox.TextDecorationsProperty, BindingMode.OneWay);
-            Bind(textBox, item, "TextWrapping", TextBox.TextWrappingProperty, BindingMode.OneWay);
-
-            // Binding for Text we have to do manually, 
-            // because the binding doesn't work properly 
-            // if focus will be remain in one of the controls
-            item.Text = textBox.Text;
-            textBox.TextChanged += delegate { item.Text = textBox.Text; };
-            item.TextChanged += delegate { textBox.Text = item.Text; };
-
-           
-            return item;
-        }
-
-        #endregion
-        
-        #region Common Stuff
-               
-        static void BindControlProperties(Control source, Control target)
-        {
-            if (source is ButtonBase)
-            {
-                Bind(source, target, "Command", ButtonBase.CommandProperty, BindingMode.OneWay);
-                Bind(source, target, "CommandParameter", ButtonBase.CommandParameterProperty, BindingMode.OneWay);
-                Bind(source, target, "CommandTarget", ButtonBase.CommandTargetProperty, BindingMode.OneWay);
-                Bind(source, target, "Command", ButtonBase.CommandProperty, BindingMode.OneWay);
-            }
-
-            Bind(source, target, "ToolTip", Control.ToolTipProperty, BindingMode.OneWay);
-            Bind(source, target, "ContextMenu", Control.ContextMenuProperty, BindingMode.OneWay);
-
-            Bind(source, target, "FontFamily", Control.FontFamilyProperty, BindingMode.OneWay);
-            Bind(source, target, "FontSize", Control.FontSizeProperty, BindingMode.OneWay);
-            Bind(source, target, "FontStretch", Control.FontStretchProperty, BindingMode.OneWay);
-            Bind(source, target, "FontStyle", Control.FontStyleProperty, BindingMode.OneWay);
-            Bind(source, target, "FontWeight", Control.FontWeightProperty, BindingMode.OneWay);
-
-            Bind(source, target, "Foreground", Control.ForegroundProperty, BindingMode.OneWay);
-            Bind(source, target, "IsEnabled", Control.IsEnabledProperty, BindingMode.OneWay);
-            Bind(source, target, "Opacity", Control.OpacityProperty, BindingMode.OneWay);
-            Bind(source, target, "SnapsToDevicePixels", Control.SnapsToDevicePixelsProperty, BindingMode.OneWay);
-            Bind(source, target, "Visibility", Control.VisibilityProperty, BindingMode.OneWay);            
-        }
-
-        #endregion
-
-        #region Binding
-
-        static void Bind(FrameworkElement source, FrameworkElement target, string path, DependencyProperty property, BindingMode mode)
-        {
-            Binding binding = new Binding();
-            binding.Path = new PropertyPath(path);
-            binding.Source = source;
-            binding.Mode = mode;
-            target.SetBinding(property, binding);
         }
 
         #endregion
