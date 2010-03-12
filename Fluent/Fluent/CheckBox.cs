6dfc98ec1b09e34210fdd7b9271f65611b2001c4
SableRaven <none@none>
2010-03-12 05:34:44 +0000

    Fixed checkbox style in qat.

--- Fluent/Fluent/CheckBox.cs
+++ Fluent/Fluent/CheckBox.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace Fluent
@@ -26,6 +27,35 @@ namespace Fluent
         public CheckBox()
         {
             
+        }
+
+        #endregion
+
+        #region Quick Access Item Creating
+
+        /// <summary>
+        /// Gets control which represents shortcut item.
+        /// This item MUST be syncronized with the original 
+        /// and send command to original one control.
+        /// </summary>
+        /// <returns>Control which represents shortcut item</returns>
+        public override FrameworkElement CreateQuickAccessItem()
+        {
+            CheckBox button = new CheckBox();
+            button.VerticalAlignment = VerticalAlignment.Center;
+            BindQuickAccessItem(button);
+            return button;
+        }
+
+        /// <summary>
+        /// This method must be overriden to bind properties to use in quick access creating
+        /// </summary>
+        /// <param name="element">Toolbar item</param>
+        protected override void BindQuickAccessItem(FrameworkElement element)
+        {
+            CheckBox button = element as CheckBox;
+            Bind(this, element, "IsChecked", IsCheckedProperty, BindingMode.TwoWay);
+            base.BindQuickAccessItem(element);
         }
 
         #endregion
