249dbdf623c7c9833260d8f4c6ae4f1c3a7bd062
firehirsch <none@none>
2013-05-20 16:40:14 +0200

    Changed build.cmd and NUSPEC-File

--- Fluent/Build.cmd
+++ Fluent/Build.cmd
@@ -3,23 +3,20 @@
 SET target=Build
 SET prerelease=
 
-:: Default to Release configuration if nothing is specified
-IF '%1'=='' (SET configuration=Release) ELSE (SET configuration=%1)
-
 :: Check if NuGet package should be built
-IF NOT '%2'=='' (
+IF NOT '%1'=='' (
     SET target=MakeNuGetPackage
 
     :: Build a prerelease package
-    IF NOT '%2'=='stable' (
-        SET prerelease=%2
+    IF NOT '%1'=='stable' (
+        SET prerelease=%1
     )
 )
 
 SET MSBuildUseNoSolutionCache=1
 SET msbuildexe="%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 
-%msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=%configuration% /property:Prerelease=%prerelease%
+%msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=Release /property:Prerelease=%prerelease%
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
 
