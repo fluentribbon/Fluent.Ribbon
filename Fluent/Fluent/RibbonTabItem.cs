a28a9b08b7764b5d843f22269ac40fb22b949ccb
batzen <none@none>
2013-07-21 22:51:19 +0200

    Force redirection of DataContext. This is needed, because we detach the container from the visual tree and attach it to a diffrent one (the popup/dropdown) when the ribbon is minimized.

--- Fluent/Fluent/RibbonTabItem.cs
+++ Fluent/Fluent/RibbonTabItem.cs
@@ -23,6 +23,7 @@ using System.Windows.Media;
 namespace Fluent
 {
     using System.Linq;
+    using System.Windows.Data;
 
     /// <summary>
     /// Represents ribbon tab item
@@ -123,12 +124,14 @@ namespace Fluent
         /// <summary>
         /// Gets an enumerator for logical child elements of this element.
         /// </summary>
-        protected override System.Collections.IEnumerator LogicalChildren
+        protected override IEnumerator LogicalChildren
         {
             get
             {
-                ArrayList array = new ArrayList();
-                array.Add(groupsContainer);
+                var array = new ArrayList
+                                {
+                                    this.groupsContainer
+                                };
                 return array.GetEnumerator();
                 /*if (Groups != null) return Groups.GetEnumerator();
                 else return (new ArrayList()).GetEnumerator();*/
@@ -185,7 +188,6 @@ namespace Fluent
         /// </summary>
         public static readonly DependencyProperty IndentProperty =
             DependencyProperty.Register("Indent", typeof(double), typeof(RibbonTabItem), new UIPropertyMetadata((double)12.0));
-
 
         /// <summary>
         /// Gets or sets whether separator is visible
@@ -465,7 +467,7 @@ namespace Fluent
         }
 
         // Coerce ToolTip to ensure that tooltip displays name of the tabitem
-        static object CoerceToolTip(DependencyObject d, object basevalue)
+        private static object CoerceToolTip(DependencyObject d, object basevalue)
         {
             var tabItem = (RibbonTabItem)d;
             if (basevalue == null
@@ -482,8 +484,15 @@ namespace Fluent
         /// </summary>
         public RibbonTabItem()
         {
-            AddLogicalChild(groupsContainer);
-            groupsContainer.Content = groupsInnerContainer;
+            this.AddLogicalChild(this.groupsContainer);
+            this.groupsContainer.Content = this.groupsInnerContainer;
+
+            // Force redirection of DataContext. This is needed, because we detach the container from the visual tree and attach it to a diffrent one (the popup/dropdown) when the ribbon is minimized.
+            this.groupsInnerContainer.SetBinding(DataContextProperty, new Binding("DataContext")
+                                                                          {
+                                                                              Source = this
+                                                                          });
+
             ContextMenuService.Coerce(this);
 
             this.Loaded += this.OnLoaded;
