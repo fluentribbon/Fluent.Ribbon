ffe85c0c45ffd2293111c57609f55b733df604b8
zhipu@vip.qq.com <zhipu@vip.qq.com>
2014-03-12 11:08:48 +0800

    Fix a bug : in Attach method add  OnWindowKeyDown to  PreviewKeyDown , but in Detach method remove OnWindowKeyDown on KeyDown.

--- Fluent/Fluent/KeyTipService.cs
+++ Fluent/Fluent/KeyTipService.cs
@@ -1,7 +1,7 @@
 #region Copyright and License Information
 // Fluent Ribbon Control Suite
 // http://fluent.codeplex.com/
-// Copyright © Degtyarev Daniel, Rikker Serg. 2009-2010.  All rights reserved.
+// Copyright ?Degtyarev Daniel, Rikker Serg. 2009-2010.  All rights reserved.
 // 
 // Distributed under the terms of the Microsoft Public License (Ms-PL). 
 // The license is available online http://fluent.codeplex.com/license
@@ -113,7 +113,7 @@ namespace Fluent
 
             if (window != null)
             {
-                window.KeyDown -= OnWindowKeyDown;
+                window.PreviewKeyDown -= OnWindowKeyDown;
                 window.KeyUp -= OnWindowKeyUp;
 
                 this.window = null;
