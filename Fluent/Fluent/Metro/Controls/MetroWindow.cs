24cb51fb6c80b6e43ea7eba1182b6b9dfb2625a0
batzen <batzen@gmx.org>
2014-06-25 23:31:21 +0200

    fixed style lookup for MetroWindow

--- Fluent/Fluent/Metro/Controls/MetroWindow.cs
+++ Fluent/Fluent/Metro/Controls/MetroWindow.cs
@@ -35,8 +35,25 @@ namespace Fluent
         }
 
         static MetroWindow()
-        {
+        {            
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroWindow), new FrameworkPropertyMetadata(typeof(MetroWindow)));
+            StyleProperty.OverrideMetadata(typeof(MetroWindow), new FrameworkPropertyMetadata(null, OnCoerceStyle));
+        }
+
+        private static object OnCoerceStyle(DependencyObject d, object basevalue)
+        {
+            if (basevalue != null)
+            {
+                return basevalue;
+            }
+
+            var frameworkElement = d as FrameworkElement;
+            if (frameworkElement != null)
+            {
+                basevalue = frameworkElement.TryFindResource(typeof(MetroWindow));
+            }
+
+            return basevalue;
         }
 
         public WindowCommands WindowCommands { get; set; }
