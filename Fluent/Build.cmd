cc4f60b61915006032a32ff20b6f7240f7792c69
batzen <batzen@gmx.org>
2014-06-27 17:37:55 +0200

    Removed pause from build file

--- Fluent/Build.cmd
+++ Fluent/Build.cmd
@@ -19,5 +19,3 @@ SET msbuildexe="%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 %msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=Release /property:Prerelease=%prerelease% /v:m /nologo
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
-
-pause
