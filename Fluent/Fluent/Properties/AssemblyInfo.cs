02873e86227a20f0d3fdb86f7a3016b630ce28aa
batzen <batzen@gmx.org>
2014-08-31 15:01:00 +0200

    Added GlobalAssemblyInfo

--- Fluent/Fluent/Properties/AssemblyInfo.cs
+++ Fluent/Fluent/Properties/AssemblyInfo.cs
@@ -6,39 +6,18 @@
 // Distributed under the terms of the Microsoft Public License (Ms-PL). 
 // The license is available online http://fluent.codeplex.com/license
 #endregion
-using System;
+
 using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
-using System.Windows;
 using System.Windows.Markup;
 
 [assembly: AssemblyTitle("Fluent")]
 [assembly: AssemblyDescription("Fluent Ribbon Control Suite")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fluent Ribbon Control Suite")]
-[assembly: AssemblyCopyright("Copyright © Degtyarev Daniel, Rikker Serg. 2009-2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
 
 [assembly: Guid("d849f751-e8f4-480d-acc0-6148eafcaafc")]
-
-[assembly: AssemblyVersion("2.1")]
-[assembly: AssemblyFileVersion("2.1")]
-[assembly: CLSCompliant(true)]
 
 [assembly: XmlnsPrefix("urn:fluent-ribbon", "fluent")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Converters")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Metro.Behaviours")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Metro.Controls")]
-
-[assembly: NeutralResourcesLanguage("en-US")]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ Fluent/Shared/GlobalAssemblyInfo.cs
@@ -0,0 +1,34 @@
+#region Copyright and License Information
+// Fluent Ribbon Control Suite
+// http://fluent.codeplex.com/
+// Copyright © Degtyarev Daniel, Rikker Serg. 2009-2013.  All rights reserved.
+// 
+// Distributed under the terms of the Microsoft Public License (Ms-PL). 
+// The license is available online http://fluent.codeplex.com/license
+#endregion
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fluent Ribbon Control Suite")]
+[assembly: AssemblyCopyright("Copyright © Degtyarev Daniel, Rikker Serg. 2009 - 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
