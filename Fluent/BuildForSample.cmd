db8b5268b10b5192715d5a578696cf783e68feb6
batzen <batzen@gmx.org>
2014-09-20 13:01:34 +0200

    Using configuration instead of prerelease

--- Fluent/BuildForSample.cmd
+++ Fluent/BuildForSample.cmd
@@ -1,13 +1,16 @@
 @ECHO OFF
 
 SET target=Build
-SET prerelease=
+SET configuration=Release
+
+IF NOT '%1'=='' (
+	SET configuration=%1
+)
 
 SET MSBuildUseNoSolutionCache=1
 SET msbuildexe="%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 
-%msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=Release /property:Prerelease=%prerelease% /v:m /nologo
-%msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=Debug /property:Prerelease=%prerelease% /v:m /nologo
+%msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=%configuration% /v:m /nologo
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
 
