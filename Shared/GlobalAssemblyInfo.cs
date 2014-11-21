b8052cbfbb7510a515325a92a2bda1dc03ec0851
batzen <batzen@gmx.org>
2014-11-21 17:55:11 +0100

    Fixes #21 by adding license, updating copyright information and updating links in nuget package to new github urls

--- Shared/GlobalAssemblyInfo.cs
+++ Shared/GlobalAssemblyInfo.cs
@@ -17,9 +17,9 @@ using System.Windows;
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/fluentribbon/Fluent.Ribbon")]
 [assembly: AssemblyProduct("Fluent Ribbon Control Suite")]
-[assembly: AssemblyCopyright("Copyright © Degtyarev Daniel, Rikker Serg. 2009 - 2014")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2014 Degtyarev Daniel, Rikker Serg, Bastian Schmidt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
