42bd643081bcd31f539cf9022b09098d1cde742d
Bastian Schmidt <batzen@gmx.org>
2015-01-30 10:10:52 +0100

    Adding sample MahMetro-Window to ensure we don't break compatibility for other Window classes like that again

---
+++ FluentTest/MahMetroWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace FluentTest
+{
+    using System;
+
+    /// <summary>
+    /// Interaction logic for MahMetroWindow.xaml
+    /// </summary>
+    [CLSCompliant(false)] // Because MetroWindow is not CLSCompliant
+    public partial class MahMetroWindow
+    {
+        public MahMetroWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
