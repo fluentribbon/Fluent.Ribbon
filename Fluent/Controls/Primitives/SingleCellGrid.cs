912a2c4d6f45440cba64172bac539588cccf6e43
Karim <abdelkarim.se@gmail.com>
2015-03-27 22:04:57 +0100

    Simplified the template of the RibbonTabItem. Replaced the usage of the Grid by the SingleCellGrid panel. lightweight and does the same job.

---
+++ Fluent/Controls/Primitives/SingleCellGrid.cs
@@ -0,0 +1,45 @@
+﻿namespace Fluent.Controls.Primitives
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// A custom panel that will arrange all of its children in the same
+    /// space.
+    /// </summary>
+    public class SingleCellGrid : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var desiredWidth = 0.0;
+            var desiredHeight = 0.0;
+
+            foreach (UIElement child in this.InternalChildren)
+            {
+                child.Measure(availableSize);
+                var desiredSize = child.DesiredSize;
+                if (desiredSize.Width > desiredWidth)
+                {
+                    desiredWidth = desiredSize.Width;
+                }
+
+                if (desiredSize.Height > desiredHeight)
+                {
+                    desiredHeight = desiredSize.Height;
+                }
+            }
+
+            return new Size(desiredWidth, desiredHeight);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var rect = new Rect(finalSize);
+            foreach (UIElement child in this.InternalChildren)
+            {
+                child.Arrange(rect);
+            }
+            return finalSize;
+        }
+    }
+}
