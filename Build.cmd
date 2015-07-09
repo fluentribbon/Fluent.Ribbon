a5015dc1b0b6102fa221a7b917deb8ba9cda90b3
batzen <batzen@gmx.org>
2015-07-09 20:20:52 +0200

    Build.cmd now only builds in debug and release mode

--- Build.cmd
+++ Build.cmd
@@ -1,23 +1,10 @@
 @ECHO OFF
 
-SET target=Build
-SET prerelease=
-
-:: Check if NuGet package should be built
-IF NOT '%1'=='' (
-    SET target=PublishVersion
-
-    :: Build a prerelease package
-    IF NOT '%1'=='stable' (
-        SET prerelease=%1
-    )
-)
-
 SET MSBuildUseNoSolutionCache=1
 SET msbuildexe="%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 
-%msbuildexe% Fluent.Ribbon.msbuild /target:Build /property:Configuration=Debug /property:Prerelease=%prerelease% /v:m /nologo
-%msbuildexe% Fluent.Ribbon.msbuild /target:%target% /property:Configuration=Release /property:Prerelease=%prerelease% /v:m /nologo
+%msbuildexe% Fluent.Ribbon.msbuild /target:Build /property:Configuration=Debug /v:m /nologo
+%msbuildexe% Fluent.Ribbon.msbuild /target:Build /property:Configuration=Release /v:m /nologo
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
 
