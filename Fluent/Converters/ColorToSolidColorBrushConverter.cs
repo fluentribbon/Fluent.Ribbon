5ad3c5409bbc88dc14318db6f670743cde06ff55
batzen <batzen@gmx.org>
2015-08-27 19:11:26 +0200

    Fixes #163 by introducing ColorToSolidColorBrushConverter

---
+++ Fluent/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,58 @@
+﻿namespace Fluent.Converters
+{
+    using System;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Converts <see cref="Color"/> to a <see cref="SolidColorBrush"/> and back.
+    /// </summary>
+    [ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+    public class ColorToSolidColorBrushValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (value is Color)
+            {
+                return new SolidColorBrush((Color)value);
+            }
+
+            throw new InvalidOperationException(string.Format("Unsupported type [{0}], ColorToSolidColorBrushValueConverter.Convert()", value.GetType().Name));
+        }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            var brush = value as SolidColorBrush;
+            if (brush != null)
+            {
+                return brush.Color;
+            }
+
+            throw new InvalidOperationException(string.Format("Unsupported type [{0}], ColorToSolidColorBrushValueConverter.Convert()", value.GetType().Name));
+        }
+    }
+}
