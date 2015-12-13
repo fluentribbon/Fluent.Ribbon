d40b8d9d45404d5663395ce56c31600b4872ee7f
Bastian Schmidt <batzen@gmx.org>
2015-12-13 11:41:15 +0100

    Bumping version to 4.0

--- Shared/GlobalAssemblyInfo.cs
+++ Shared/GlobalAssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 [assembly: AssemblyInformationalVersion("SRC")]
 
 [assembly: ComVisible(false)]
