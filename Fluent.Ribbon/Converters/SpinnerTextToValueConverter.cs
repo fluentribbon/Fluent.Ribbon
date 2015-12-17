60f0a34f9aa708d45c37eb6c7a9be84668643c92
Bastian Schmidt <batzen@gmx.org>
2015-12-18 00:44:05 +0100

    Adding missing documentation

--- Fluent.Ribbon/Converters/SpinnerTextToValueConverter.cs
+++ Fluent.Ribbon/Converters/SpinnerTextToValueConverter.cs
@@ -5,8 +5,18 @@
     using System.Text;
     using System.Windows.Data;
 
+    /// <summary>
+    /// Converter class which converts from <see cref="string"/> to <see cref="double"/> and back.
+    /// </summary>
     public class SpinnerTextToValueConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
         public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var converterParam = (Tuple<string, double>)parameter;
@@ -16,11 +26,22 @@
             return this.TextToDouble((string)value, format, previousValue, culture);
         }
 
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return this.DoubleToText((double)value, (string)parameter, culture);
         }
 
+        /// <summary>
+        /// Converts the given <paramref name="text"/> to a <see cref="double"/>.
+        /// </summary>
+        /// <returns>The <see cref="double"/> value converted from <paramref name="text"/> or <paramref name="previousValue"/> if the conversion fails.</returns>
         public virtual double TextToDouble(string text, string format, double previousValue, CultureInfo culture)
         {
             // Remove all except digits, signs and commas
@@ -49,6 +70,10 @@
             return doubleValue;
         }
 
+        /// <summary>
+        /// Converts <paramref name="value"/> to a formatted text using <paramref name="format"/>.
+        /// </summary>
+        /// <returns><paramref name="value"/> converted to a <see cref="string"/>.</returns>
         public virtual string DoubleToText(double value, string format, CultureInfo culture)
         {
             return value.ToString(format, culture);
