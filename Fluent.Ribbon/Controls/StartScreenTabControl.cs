661e9bd2080c74ad1e145bd279e4fd07094bfcba
Bastian Schmidt <batzen@gmx.org>
2016-01-23 19:26:37 +0100

    Fixing layout of StartScreenTabControl on Office 2010 and Windows 8 themes

--- Fluent.Ribbon/Controls/StartScreenTabControl.cs
+++ Fluent.Ribbon/Controls/StartScreenTabControl.cs
@@ -38,6 +38,36 @@
             DependencyProperty.Register(nameof(RightContent), typeof(object), typeof(StartScreenTabControl));
 
         /// <summary>
+        /// Defines the margin for <see cref="LeftContent"/>
+        /// </summary>
+        public Thickness LeftContentMargin
+        {
+            get { return (Thickness)this.GetValue(LeftContentMarginProperty); }
+            set { this.SetValue(LeftContentMarginProperty, value); }
+        }
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> for <see cref="LeftContentMargin"/>.
+        /// </summary>
+        public static readonly DependencyProperty LeftContentMarginProperty =
+            DependencyProperty.Register(nameof(LeftContentMargin), typeof(Thickness), typeof(StartScreenTabControl), new PropertyMetadata(default(Thickness)));
+
+        /// <summary>
+        /// Defines if the <see cref="WindowSteeringHelperControl"/> is enabled in this control
+        /// </summary>
+        public bool IsWindowSteeringHelperEnabled
+        {
+            get { return (bool)this.GetValue(IsWindowSteeringHelperEnabledProperty); }
+            set { this.SetValue(IsWindowSteeringHelperEnabledProperty, value); }
+        }
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> for <see cref="IsWindowSteeringHelperEnabled"/>.
+        /// </summary>
+        public static readonly DependencyProperty IsWindowSteeringHelperEnabledProperty =
+            DependencyProperty.Register(nameof(IsWindowSteeringHelperEnabled), typeof(bool), typeof(StartScreenTabControl), new PropertyMetadata(true));
+
+        /// <summary>
         /// Static constructor.
         /// </summary>
         static StartScreenTabControl()
