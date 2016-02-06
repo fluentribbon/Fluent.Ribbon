0695c0cb5600afb438b73b270f977ee41fa91107
Bastian Schmidt <batzen@gmx.org>
2016-02-06 16:49:38 +0100

    Code style and slight performance tweak

--- Fluent.Ribbon/Controls/TwoLineLabel.cs
+++ Fluent.Ribbon/Controls/TwoLineLabel.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-
-namespace Fluent
+﻿namespace Fluent
 {
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Markup;
+
     /// <summary>
     /// Represents specific label to use in particular ribbon controls
     /// </summary>
+    [DefaultProperty(nameof(Text))]
+    [ContentProperty(nameof(Text))]
     [TemplatePart(Name = "PART_TextRun", Type = typeof(TextBlock))]
     [TemplatePart(Name = "PART_TextRun2", Type = typeof(TextBlock))]
     [TemplatePart(Name = "PART_Glyph", Type = typeof(InlineUIContainer))]
-    public class TwoLineLabel: Control
+    public class TwoLineLabel : Control
     {
         #region Fields
 
@@ -41,7 +45,7 @@ namespace Fluent
         /// This enables animation, styling, binding, etc...
         /// </summary>
         public static readonly DependencyProperty HasTwoLinesProperty =
-            DependencyProperty.Register("HasTwoLines", typeof(bool), typeof(TwoLineLabel), new UIPropertyMetadata(true,OnHasTwoLinesChanged));
+            DependencyProperty.Register("HasTwoLines", typeof(bool), typeof(TwoLineLabel), new UIPropertyMetadata(true, OnHasTwoLinesChanged));
 
         /// <summary>
         /// Handles HasTwoLines property changes
@@ -50,7 +54,7 @@ namespace Fluent
         /// <param name="e">The event data</param>
         private static void OnHasTwoLinesChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            (d as TwoLineLabel).UpdateTextRun();
+            ((TwoLineLabel)d).UpdateTextRun();
         }
 
         /// <summary>
@@ -75,7 +79,7 @@ namespace Fluent
         /// <param name="e">The event data</param>
         private static void OnHasGlyphChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            (d as TwoLineLabel).UpdateTextRun();
+            ((TwoLineLabel)d).UpdateTextRun();
         }
 
         /// <summary>
@@ -91,7 +95,7 @@ namespace Fluent
         /// Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
         /// </summary>
         public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register("Text", typeof(string), typeof(TwoLineLabel), new UIPropertyMetadata("TwoLineLabel", OnTextChanged));
+            DependencyProperty.Register("Text", typeof(string), typeof(TwoLineLabel), new UIPropertyMetadata(string.Empty, OnTextChanged));
 
         #endregion
 
@@ -104,11 +108,11 @@ namespace Fluent
         static TwoLineLabel()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TwoLineLabel), new FrameworkPropertyMetadata(typeof(TwoLineLabel)));
-            StyleProperty.OverrideMetadata(typeof(TwoLineLabel), new FrameworkPropertyMetadata(null, new CoerceValueCallback(OnCoerceStyle)));
+            StyleProperty.OverrideMetadata(typeof(TwoLineLabel), new FrameworkPropertyMetadata(null, OnCoerceStyle));
         }
 
         // Coerce object style
-        static object OnCoerceStyle(DependencyObject d, object basevalue)
+        private static object OnCoerceStyle(DependencyObject d, object basevalue)
         {
             if (basevalue == null)
             {
@@ -152,8 +156,8 @@ namespace Fluent
         /// <param name="e">The event data</param>
         private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            TwoLineLabel label = d as TwoLineLabel;
-            label.UpdateTextRun();
+            var label = (TwoLineLabel)d;
+            label?.UpdateTextRun();
         }
 
         #endregion
@@ -163,59 +167,69 @@ namespace Fluent
         /// <summary>
         /// Updates text run adds newline if HasTwoLines == true
         /// </summary>
-        void UpdateTextRun()
+        private void UpdateTextRun()
         {
-            if ((this.textRun != null)&&(this.textRun2 != null)&&(this.Text != null))
+            if (this.textRun == null
+                || this.textRun2 == null)
+            {
+                return;
+            }
+
+            if (this.HasTwoLines == false
+                || string.IsNullOrEmpty(this.Text))
             {
                 this.textRun.Text = this.Text;
-                this.textRun2.Text = "";
-                string text = this.Text.Trim();
-                if (this.HasTwoLines)
+                this.textRun2.Text = string.Empty;
+                return;
+            }
+
+            var text = this.Text.Trim();
+
+            // Find soft hyphen, break at its position and display a normal hyphen.
+            var hyphenIndex = text.IndexOf((char)173);
+
+            if (hyphenIndex >= 0)
+            {
+                this.textRun.Text = text.Substring(0, hyphenIndex) + "-";
+                this.textRun2.Text = text.Substring(hyphenIndex) + " ";
+            }
+            else
+            {
+                var centerIndex = this.Text.Length / 2;
+                // Find spaces nearest to center from left and right
+                var leftSpaceIndex = text.LastIndexOf(" ", centerIndex, centerIndex, StringComparison.CurrentCulture);
+                var rightSpaceIndex = text.IndexOf(" ", centerIndex, StringComparison.CurrentCulture);
+
+                if (leftSpaceIndex == -1
+                    && rightSpaceIndex == -1)
                 {
-                    // Find soft hyphen, break at its position and display a normal hyphen.
-                    int hyphenIndex = text.IndexOf((char)173);
-                    if (hyphenIndex >= 0)
+                    this.textRun.Text = this.Text;
+                    this.textRun2.Text = string.Empty;
+                }
+                else if (leftSpaceIndex == -1)
+                {
+                    // Finds only space from right. New line adds on it
+                    this.textRun.Text = text.Substring(0, rightSpaceIndex);
+                    this.textRun2.Text = text.Substring(rightSpaceIndex) + " ";
+                }
+                else if (rightSpaceIndex == -1)
+                {
+                    // Finds only space from left. New line adds on it
+                    this.textRun.Text = text.Substring(0, leftSpaceIndex);
+                    this.textRun2.Text = text.Substring(leftSpaceIndex) + " ";
+                }
+                else
+                {
+                    // Find nearest to center space and add new line on it
+                    if (Math.Abs(centerIndex - leftSpaceIndex) < Math.Abs(centerIndex - rightSpaceIndex))
                     {
-                        this.textRun.Text = text.Substring(0, hyphenIndex) + "-";
-                        this.textRun2.Text = text.Substring(hyphenIndex) + " ";
+                        this.textRun.Text = text.Substring(0, leftSpaceIndex);
+                        this.textRun2.Text = text.Substring(leftSpaceIndex) + " ";
                     }
                     else
                     {
-                        int centerIndex = this.Text.Length / 2;
-                        // Find spaces nearest to center from left and right
-                        int leftSpaceIndex = text.LastIndexOf(" ", centerIndex, centerIndex);
-                        int rightSpaceIndex = text.IndexOf(" ", centerIndex, StringComparison.CurrentCulture);
-                        if ((leftSpaceIndex == -1) && (rightSpaceIndex == -1))
-                        {
-                            // The text can`t be separated. Add new line for glyph
-                            //textRun.Text += '\u0085';
-                        }
-                        else if (leftSpaceIndex == -1)
-                        {
-                            // Finds only space from right. New line adds on it
-                            this.textRun.Text = text.Substring(0, rightSpaceIndex);
-                            this.textRun2.Text = text.Substring(rightSpaceIndex) + " ";
-                        }
-                        else if (rightSpaceIndex == -1)
-                        {
-                            // Finds only space from left. New line adds on it
-                            this.textRun.Text = text.Substring(0, leftSpaceIndex);
-                            this.textRun2.Text = text.Substring(leftSpaceIndex) + " ";
-                        }
-                        else
-                        {
-                            // Find nearest to center space and add new line on it
-                            if (Math.Abs(centerIndex - leftSpaceIndex) < Math.Abs(centerIndex - rightSpaceIndex))
-                            {
-                                this.textRun.Text = text.Substring(0, leftSpaceIndex);
-                                this.textRun2.Text = text.Substring(leftSpaceIndex) + " ";
-                            }
-                            else
-                            {
-                                this.textRun.Text = text.Substring(0, rightSpaceIndex);
-                                this.textRun2.Text = text.Substring(rightSpaceIndex) + " ";
-                            }
-                        }
+                        this.textRun.Text = text.Substring(0, rightSpaceIndex);
+                        this.textRun2.Text = text.Substring(rightSpaceIndex) + " ";
                     }
                 }
             }
