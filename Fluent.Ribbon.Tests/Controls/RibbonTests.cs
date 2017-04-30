73c092b06873a6fa38634c26ecdc16b68220b3ee
Bastian Schmidt <batzen@gmx.org>
2017-04-30 21:14:40 +0200

    Optimize UnitTest

--- Fluent.Ribbon.Tests/Controls/RibbonTests.cs
+++ Fluent.Ribbon.Tests/Controls/RibbonTests.cs
@@ -1,5 +1,8 @@
 ﻿namespace Fluent.Tests.Controls
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
     using System.Windows.Markup;
     using Fluent.Tests.TestClasses;
     using NUnit.Framework;
@@ -27,77 +30,43 @@
             {
                 ribbon.ApplyTemplate();
 
-                {
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(window.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(window.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(window.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(window.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(window.Language), "Language on TabControl should match.");
+                var elemens = new Dictionary<FrameworkElement, string>
+                              {
+                                  { ribbon, "Ribbon" },
+                                  { ribbon.Menu, "Menu" },
+                                  { ribbon.StartScreen, "StartScreen" },
+                                  { ribbon.QuickAccessToolBar, "QuickAccessToolBar" },
+                                  { ribbon.TabControl, "TabControl" },
+                                  { (FrameworkElement)ribbon.Template.FindName("PART_LayoutRoot", ribbon), "PART_LayoutRoot" },
+                              };
 
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(window.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(window.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(window.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(window.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(window.DataContext), "DataContext on TabControl should match.");
+                {
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, window);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, window);
                 }
 
                 {
                     window.Language = enUs;
                     window.DataContext = window.Language;
 
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(window.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(window.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(window.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(window.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(window.Language), "Language on TabControl should match.");
-
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(window.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(window.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(window.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(window.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(window.DataContext), "DataContext on TabControl should match.");
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, window);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, window);
                 }
 
                 {
                     window.Language = deDe;
                     window.DataContext = window.Language;
 
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(window.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(window.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(window.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(window.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(window.Language), "Language on TabControl should match.");
-
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(window.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(window.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(window.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(window.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(window.DataContext), "DataContext on TabControl should match.");
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, window);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, window);
                 }
 
                 {
                     window.Language = enUs;
                     window.DataContext = window.Language;
 
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(window.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(window.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(window.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(window.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(window.Language), "Language on TabControl should match.");
-
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(window.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(window.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(window.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(window.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(window.DataContext), "DataContext on TabControl should match.");
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, window);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, window);
                 }
             }
         }
@@ -122,79 +91,67 @@
             {
                 ribbon.ApplyTemplate();
 
-                {
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(ribbon.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(ribbon.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(ribbon.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(ribbon.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(ribbon.Language), "Language on TabControl should match.");
+                var elemens = new Dictionary<FrameworkElement, string>
+                              {
+                                  { ribbon, "Ribbon" },
+                                  { ribbon.Menu, "Menu" },
+                                  { ribbon.StartScreen, "StartScreen" },
+                                  { ribbon.QuickAccessToolBar, "QuickAccessToolBar" },
+                                  { ribbon.TabControl, "TabControl" },
+                                  { (FrameworkElement)ribbon.Template.FindName("PART_LayoutRoot", ribbon), "PART_LayoutRoot" },
+                              };
 
-                    Assert.That(window.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on TabControl should match.");
+                {
+                    Assert.That(ribbon.Language, Is.EqualTo(window.Language), "Language on Window should match.");
+                    Assert.That(ribbon.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
+
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, ribbon);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, ribbon);
                 }
 
                 {
                     ribbon.Language = enUs;
                     ribbon.DataContext = ribbon.Language;
 
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(ribbon.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(ribbon.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(ribbon.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(ribbon.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(ribbon.Language), "Language on TabControl should match.");
+                    Assert.That(ribbon.Language, Is.Not.EqualTo(window.Language), "Language on Ribbon should not match Window.");
+                    Assert.That(ribbon.DataContext, Is.Not.EqualTo(window.DataContext), "DataContext on Ribbon should not match Window.");
 
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on TabControl should match.");
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, ribbon);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, ribbon);
                 }
 
                 {
                     ribbon.Language = deDe;
                     ribbon.DataContext = ribbon.Language;
 
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(ribbon.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(ribbon.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(ribbon.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(ribbon.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(ribbon.Language), "Language on TabControl should match.");
+                    Assert.That(ribbon.Language, Is.EqualTo(window.Language), "Language on Ribbon should match Window.");
+                    Assert.That(ribbon.DataContext, Is.EqualTo(window.DataContext), "DataContext on Ribbon should match Window.");
 
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on TabControl should match.");
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, ribbon);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, ribbon);
                 }
 
                 {
                     ribbon.Language = enUs;
                     ribbon.DataContext = ribbon.Language;
 
-                    Assert.That(window.Language, Is.EqualTo(window.Language), "Language on Window should match.");
-                    Assert.That(ribbon.Language, Is.EqualTo(ribbon.Language), "Language on Ribbon should match.");
-                    Assert.That(ribbon.Menu.Language, Is.EqualTo(ribbon.Language), "Language on Menu should match.");
-                    Assert.That(ribbon.StartScreen.Language, Is.EqualTo(ribbon.Language), "Language on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.Language, Is.EqualTo(ribbon.Language), "Language on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.Language, Is.EqualTo(ribbon.Language), "Language on TabControl should match.");
+                    Assert.That(ribbon.Language, Is.Not.EqualTo(window.Language), "Language on Ribbon should not match Window.");
+                    Assert.That(ribbon.DataContext, Is.Not.EqualTo(window.DataContext), "DataContext on Ribbon should not match Window.");
 
-                    Assert.That(window.DataContext, Is.EqualTo(window.DataContext), "DataContext on Window should match.");
-                    Assert.That(ribbon.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Ribbon should match.");
-                    Assert.That(ribbon.Menu.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on Menu should match.");
-                    Assert.That(ribbon.StartScreen.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on StartScreen should match.");
-                    Assert.That(ribbon.QuickAccessToolBar.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on QuickAccessToolBar should match.");
-                    Assert.That(ribbon.TabControl.DataContext, Is.EqualTo(ribbon.DataContext), "DataContext on TabControl should match.");
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.DataContextProperty, ribbon);
+                    AssertAttElementsHaveSameValue(elemens, FrameworkElement.LanguageProperty, ribbon);
                 }
             }
+        }
+
+        private static void AssertAttElementsHaveSameValue(Dictionary<FrameworkElement, string> elements, DependencyProperty property, FrameworkElement expectedValueSource)
+        {
+            var expectedValue = expectedValueSource.GetValue(property);
+
+            foreach (var element in elements)
+            {
+                Assert.That(element.Key.GetValue(property), Is.EqualTo(expectedValue), $"{property.Name} on {element.Value} should match.");
+            }            
         }
     }
 }
