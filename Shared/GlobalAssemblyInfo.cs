8c2cdc59e7427511eb44ef7e75571b3ef502b36f
Bastian Schmidt <batzen@gmx.org>
2018-02-04 15:31:33 +0100

    Fixing assembly version

--- Shared/GlobalAssemblyInfo.cs
+++ Shared/GlobalAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("6.1.0")]
-[assembly: AssemblyFileVersion("6.1.0")]
+[assembly: AssemblyVersion("6.1.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
 [assembly: AssemblyInformationalVersion("SRC")]
 
 [assembly: ComVisible(false)]
