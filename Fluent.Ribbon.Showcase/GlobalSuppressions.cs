6b4c1fa277c75591dea848cf6b1332a6f652d900
Bastian Schmidt <batzen@gmx.org>
2019-01-04 14:30:53 +0100

    Adding proper justification

--- Fluent.Ribbon.Showcase/GlobalSuppressions.cs
+++ Fluent.Ribbon.Showcase/GlobalSuppressions.cs
@@ -3,4 +3,4 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "xxxxx")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1652:Enable XML documentation output", Justification = "No xml documentation is needed for showcase projects.")]
