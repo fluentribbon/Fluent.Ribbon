f63e256564466cdd61494f85073a0c6106be3af8
Bastian Schmidt <batzen@gmx.org>
2019-03-02 11:11:14 +0100

    Adding scope guard class to prevent reentrancy

---
+++ Fluent.Ribbon/Internal/ScopeGuard.cs
@@ -0,0 +1,21 @@
+﻿namespace Fluent.Internal
+{
+    using System;
+
+    /// <summary>
+    /// Scope guard to prevent reentrancy.
+    /// </summary>
+    public class ScopeGuard : IDisposable
+    {
+        /// <summary>
+        /// Gets whether this instance is still active (not disposed) or not.
+        /// </summary>
+        public bool IsActive { get; private set; } = true;
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            this.IsActive = false;
+        }
+    }
+}
