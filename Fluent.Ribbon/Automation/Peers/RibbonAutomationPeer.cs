5b9a1c9d3ad59d8730ed8030e9d87b885043b04c
Bastian Schmidt <batzen@gmx.org>
2019-09-19 20:23:48 +0200

    Adding IExpandCollapseProvider to Ribbon peer

--- Fluent.Ribbon/Automation/Peers/RibbonAutomationPeer.cs
+++ Fluent.Ribbon/Automation/Peers/RibbonAutomationPeer.cs
@@ -1,8 +1,10 @@
-﻿namespace Fluent.Automation.Peers
+namespace Fluent.Automation.Peers
 {
     using System.Collections.Generic;
     using System.Windows;
+    using System.Windows.Automation;
     using System.Windows.Automation.Peers;
+    using System.Windows.Automation.Provider;
     using System.Windows.Controls;
     using Fluent.Extensions;
     using JetBrains.Annotations;
@@ -10,7 +12,7 @@
     /// <summary>
     /// Automation peer for <see cref="Ribbon"/>.
     /// </summary>
-    public class RibbonAutomationPeer : FrameworkElementAutomationPeer
+    public class RibbonAutomationPeer : FrameworkElementAutomationPeer, IExpandCollapseProvider
     {
         private Ribbon OwningRibbon => (Ribbon)this.Owner;
 
@@ -49,6 +51,12 @@
         /// <inheritdoc />
         protected override List<AutomationPeer> GetChildrenCore()
         {
+            // If Ribbon is Collapsed, dont show anything in the UIA tree
+            if (this.OwningRibbon.IsCollapsed)
+            {
+                return null;
+            }
+
             var list = new List<AutomationPeer>();
             if (this.OwningRibbon.QuickAccessToolBar != null)
             {
@@ -103,6 +111,36 @@
             }
 
             return list;
+        }
+
+        /// <inheritdoc/>
+        protected override bool IsOffscreenCore()
+        {
+            return this.OwningRibbon.IsCollapsed 
+                   || base.IsOffscreenCore();
+        }
+
+        /// <inheritdoc />
+        public void Collapse()
+        {
+            this.OwningRibbon.IsMinimized = true;
+        }
+
+        /// <inheritdoc />
+        public void Expand()
+        {
+            this.OwningRibbon.IsMinimized = false;
+        }
+
+        /// <inheritdoc />
+        public ExpandCollapseState ExpandCollapseState => this.OwningRibbon.IsMinimized ? ExpandCollapseState.Collapsed : ExpandCollapseState.Expanded;
+
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
+        internal void RaiseExpandCollapseAutomationEvent(bool oldValue, bool newValue)
+        {
+            this.RaisePropertyChangedEvent(ExpandCollapsePatternIdentifiers.ExpandCollapseStateProperty,
+                                      oldValue ? ExpandCollapseState.Expanded : ExpandCollapseState.Collapsed,
+                                      newValue ? ExpandCollapseState.Expanded : ExpandCollapseState.Collapsed);
         }
     }
 }
