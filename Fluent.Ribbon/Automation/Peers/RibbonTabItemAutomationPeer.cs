1bdd6577d759d615bfefde6801090e733f0fcf36
Bastian Schmidt <batzen@gmx.org>
2019-09-19 20:22:25 +0200

    Removing unneeded code from tab item peer

--- Fluent.Ribbon/Automation/Peers/RibbonTabItemAutomationPeer.cs
+++ Fluent.Ribbon/Automation/Peers/RibbonTabItemAutomationPeer.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.Runtime.CompilerServices;
-    using System.Windows;
     using System.Windows.Automation;
     using System.Windows.Automation.Peers;
     using JetBrains.Annotations;
@@ -13,23 +12,6 @@
     public class RibbonTabItemAutomationPeer : FrameworkElementAutomationPeer
     {
         private RibbonTabItem OwningTab => (RibbonTabItem)this.Owner;
-
-        //private RibbonTabHeaderDataAutomationPeer HeaderPeer
-        //{
-        //    get
-        //    {
-        //        if (this.OwningTab.Header != null)
-        //        {
-        //            var automationPeer = UIElementAutomationPeer.CreatePeerForElement(this.OwningTab.Header);
-        //            if (automationPeer != null)
-        //            {
-        //                return automationPeer.EventsSource as RibbonTabHeaderDataAutomationPeer;
-        //            }
-        //        }
-
-        //        return null;
-        //    }
-        //}
 
         /// <summary>
         /// Creates a new instance.
@@ -58,16 +40,6 @@
                 }
             }
 
-            //if (HeaderPeer != null)
-            //{
-            //    if (list == null)
-            //    {
-            //        list = new List<AutomationPeer>(1);
-            //    }
-
-            //    list.Insert(0, HeaderPeer);
-            //}
-
             return list;
         }
 
@@ -76,36 +48,6 @@
         {
             return this.Owner.GetType().Name;
         }
-
-        /// <inheritdoc />
-        protected override Rect GetBoundingRectangleCore()
-        {
-            //if (!this.OwningTab.IsSelected && HeaderPeer != null)
-            //{
-            //    return HeaderPeer.GetBoundingRectangle();
-            //}
-            Rect boundingRectangleCore = base.GetBoundingRectangleCore();
-            //if (HeaderPeer != null)
-            //{
-            //    boundingRectangleCore.Union(HeaderPeer.GetBoundingRectangle());
-            //}
-            return boundingRectangleCore;
-        }
-
-        /// <inheritdoc />
-        //protected override ItemAutomationPeer CreateItemAutomationPeer(object item)
-        //{
-        //    return new RibbonGroupBoxDataAutomationPeer(item, this);
-        //}
-
-        //internal override Rect GetVisibleBoundingRectCore()
-        //{
-        //    if (!this.OwningTab.IsSelected && HeaderPeer != null)
-        //    {
-        //        return HeaderPeer.GetVisibleBoundingRect();
-        //    }
-        //    return base.GetVisibleBoundingRectCore();
-        //}
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         internal void RaiseTabExpandCollapseAutomationEvent(bool oldValue, bool newValue)
@@ -116,7 +58,7 @@
         [MethodImpl(MethodImplOptions.NoInlining)]
         internal void RaiseTabSelectionEvents()
         {
-            AutomationPeer eventsSource = this.EventsSource;
+            var eventsSource = this.EventsSource;
             if (eventsSource != null)
             {
                 if (this.OwningTab.IsSelected)
