909216466df809876968b810c2330b84362db296
Bastian Schmidt <batzen@gmx.org>
2019-09-20 11:50:59 +0200

    Disabling code for group box data peer

--- Fluent.Ribbon/Automation/Peers/RibbonGroupBoxDataAutomationPeer.cs
+++ Fluent.Ribbon/Automation/Peers/RibbonGroupBoxDataAutomationPeer.cs
@@ -1,122 +1,122 @@
-﻿namespace Fluent.Automation.Peers
-{
-    using System.Windows.Automation;
-    using System.Windows.Automation.Peers;
-    using System.Windows.Automation.Provider;
-    using Fluent.Extensions;
-    using JetBrains.Annotations;
+﻿//namespace Fluent.Automation.Peers
+//{
+//    using System.Windows.Automation;
+//    using System.Windows.Automation.Peers;
+//    using System.Windows.Automation.Provider;
+//    using Fluent.Extensions;
+//    using JetBrains.Annotations;
 
-    /// <summary>
-    ///     Automation peer wrapper for <see cref="RibbonGroupBox" />.
-    /// </summary>
-    public class RibbonGroupBoxDataAutomationPeer : ItemAutomationPeer, IScrollItemProvider, IExpandCollapseProvider
-    {
-        /// <summary>
-        ///     Creates a new instance.
-        /// </summary>
-        public RibbonGroupBoxDataAutomationPeer(object item, [NotNull] RibbonTabItemAutomationPeer itemsControlPeer)
-            : base(item, itemsControlPeer)
-        {
-        }
+//    /// <summary>
+//    ///     Automation peer wrapper for <see cref="RibbonGroupBox" />.
+//    /// </summary>
+//    public class RibbonGroupBoxDataAutomationPeer : ItemAutomationPeer, IScrollItemProvider, IExpandCollapseProvider
+//    {
+//        /// <summary>
+//        ///     Creates a new instance.
+//        /// </summary>
+//        public RibbonGroupBoxDataAutomationPeer(object item, [NotNull] RibbonTabItemAutomationPeer itemsControlPeer)
+//            : base(item, itemsControlPeer)
+//        {
+//        }
 
-        ExpandCollapseState IExpandCollapseProvider.ExpandCollapseState
-        {
-            get
-            {
-                var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
-                if (ribbonGroup != null
-                    && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
-                {
-                    if (!ribbonGroup.IsDropDownOpen)
-                    {
-                        return ExpandCollapseState.Collapsed;
-                    }
+//        ExpandCollapseState IExpandCollapseProvider.ExpandCollapseState
+//        {
+//            get
+//            {
+//                var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
+//                if (ribbonGroup != null
+//                    && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
+//                {
+//                    if (!ribbonGroup.IsDropDownOpen)
+//                    {
+//                        return ExpandCollapseState.Collapsed;
+//                    }
 
-                    return ExpandCollapseState.Expanded;
-                }
+//                    return ExpandCollapseState.Expanded;
+//                }
 
-                return ExpandCollapseState.LeafNode;
-            }
-        }
+//                return ExpandCollapseState.LeafNode;
+//            }
+//        }
 
-        /// <inheritdoc />
-        void IExpandCollapseProvider.Collapse()
-        {
-            var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
-            if (ribbonGroup != null
-                && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
-            {
-                ribbonGroup.IsDropDownOpen = false;
-            }
-        }
+//        /// <inheritdoc />
+//        void IExpandCollapseProvider.Collapse()
+//        {
+//            var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
+//            if (ribbonGroup != null
+//                && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
+//            {
+//                ribbonGroup.IsDropDownOpen = false;
+//            }
+//        }
 
-        /// <inheritdoc />
-        void IExpandCollapseProvider.Expand()
-        {
-            var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
-            if (ribbonGroup != null
-                && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
-            {
-                ribbonGroup.IsDropDownOpen = true;
-            }
-        }
+//        /// <inheritdoc />
+//        void IExpandCollapseProvider.Expand()
+//        {
+//            var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
+//            if (ribbonGroup != null
+//                && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
+//            {
+//                ribbonGroup.IsDropDownOpen = true;
+//            }
+//        }
 
-        /// <inheritdoc />
-        void IScrollItemProvider.ScrollIntoView()
-        {
-            (this.GetWrapper() as RibbonGroupBox)?.BringIntoView();
-        }
+//        /// <inheritdoc />
+//        void IScrollItemProvider.ScrollIntoView()
+//        {
+//            (this.GetWrapper() as RibbonGroupBox)?.BringIntoView();
+//        }
 
-        /// <inheritdoc />
-        public override object GetPattern(PatternInterface patternInterface)
-        {
-            object obj = null;
-            switch (patternInterface)
-            {
-                case PatternInterface.ScrollItem:
-                    obj = this;
-                    break;
-                case PatternInterface.ExpandCollapse:
-                {
-                    var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
-                    if (ribbonGroup != null
-                        && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
-                    {
-                        obj = this;
-                    }
+//        /// <inheritdoc />
+//        public override object GetPattern(PatternInterface patternInterface)
+//        {
+//            object obj = null;
+//            switch (patternInterface)
+//            {
+//                case PatternInterface.ScrollItem:
+//                    obj = this;
+//                    break;
+//                case PatternInterface.ExpandCollapse:
+//                {
+//                    var ribbonGroup = this.GetWrapper() as RibbonGroupBox;
+//                    if (ribbonGroup != null
+//                        && ribbonGroup.State == RibbonGroupBoxState.Collapsed)
+//                    {
+//                        obj = this;
+//                    }
 
-                    break;
-                }
-            }
+//                    break;
+//                }
+//            }
 
-            if (obj == null)
-            {
-                var wrapperPeer = this.GetWrapperPeer();
-                if (wrapperPeer != null)
-                {
-                    obj = wrapperPeer.GetPattern(patternInterface);
-                }
-            }
+//            if (obj == null)
+//            {
+//                var wrapperPeer = this.GetWrapperPeer();
+//                if (wrapperPeer != null)
+//                {
+//                    obj = wrapperPeer.GetPattern(patternInterface);
+//                }
+//            }
 
-            return obj;
-        }
+//            return obj;
+//        }
 
-        /// <inheritdoc />
-        protected override AutomationControlType GetAutomationControlTypeCore()
-        {
-            return AutomationControlType.Group;
-        }
+//        /// <inheritdoc />
+//        protected override AutomationControlType GetAutomationControlTypeCore()
+//        {
+//            return AutomationControlType.Group;
+//        }
 
-        /// <inheritdoc />
-        protected override string GetClassNameCore()
-        {
-            var wrapperPeer = this.GetWrapperPeer();
-            if (wrapperPeer != null)
-            {
-                return wrapperPeer.GetClassName();
-            }
+//        /// <inheritdoc />
+//        protected override string GetClassNameCore()
+//        {
+//            var wrapperPeer = this.GetWrapperPeer();
+//            if (wrapperPeer != null)
+//            {
+//                return wrapperPeer.GetClassName();
+//            }
 
-            return string.Empty;
-        }
-    }
-}
+//            return string.Empty;
+//        }
+//    }
+//}
