011f5d3dfbd231194fb2ed66cf444f6784b6b764
Bastian Schmidt <batzen@gmx.org>
2019-12-01 15:22:22 +0100

    Improving code quality

---
+++ Fluent.Ribbon/Internal/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Fluent.Internal
+{
+    using System.Runtime.CompilerServices;
+    using JetBrains.Annotations;
+
+    internal static class ObjectExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [ContractAnnotation("obj:null => true")]
+        public static bool IsNull(this object obj)
+        {
+            return obj is null;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [ContractAnnotation("obj:null => false")]
+        public static bool IsNotNull(this object obj)
+        {
+            return !(obj is null);
+        }
+    }
+}
