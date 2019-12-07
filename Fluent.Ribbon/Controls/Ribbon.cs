9587a6cc87ee0ee676ab076004e467bc4544c177
Bastian Schmidt <batzen@gmx.org>
2019-12-07 17:00:01 +0100

    We have to force a measure on the titlebar once we are loaded
    This also fixes #751

--- Fluent.Ribbon/Controls/Ribbon.cs
+++ Fluent.Ribbon/Controls/Ribbon.cs
@@ -15,6 +15,7 @@ namespace Fluent
     using System.Windows.Data;
     using System.Windows.Input;
     using System.Windows.Markup;
+    using Fluent.Extensions;
     using Fluent.Internal.KnownBoxes;
     using Fluent.Localization;
     using WindowChrome = ControlzEx.Windows.Shell.WindowChrome;
@@ -1968,6 +1969,8 @@ namespace Fluent
             this.AttachToWindow();
 
             this.LoadInitialState();
+
+            this.TitleBar?.ForceMeasureAndArrange();
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
