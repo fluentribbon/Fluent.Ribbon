34d7bb86079e598713abc393f2899dd1f2e497a3
Bastian Schmidt <batzen@gmx.org>
2020-02-23 12:09:08 +0100

    Switching to CI version of ControlzEx

--- Fluent.Ribbon/Theming/RibbonLibraryThemeProvider.cs
+++ Fluent.Ribbon/Theming/RibbonLibraryThemeProvider.cs
@@ -1,16 +1,16 @@
 namespace Fluent.Theming
 {
     using System.Collections.Generic;
-    using System.Windows.Media;
     using ControlzEx.Theming;
-
-#pragma warning disable CS1591
 
     /// <summary>
     /// Provides theme resources from Fluent.Ribbon.
     /// </summary>
     public class RibbonLibraryThemeProvider : LibraryThemeProvider
     {
+        /// <summary>
+        /// Gets the default instance of this class.
+        /// </summary>
         public static readonly RibbonLibraryThemeProvider DefaultInstance = new RibbonLibraryThemeProvider();
 
         /// <inheritdoc cref="LibraryThemeProvider" />
@@ -20,16 +20,16 @@ namespace Fluent.Theming
         }
 
         /// <inheritdoc />
-        public override void FillColorSchemeValues(Dictionary<string, string> values, Color accentColor, Color accentColor80Percent, Color accentColor60Percent, Color accentColor40Percent, Color accentColor20Percent, Color highlightColor, Color idealForegroundColor)
+        public override void FillColorSchemeValues(Dictionary<string, string> values, RuntimeThemeColorValues colorValues)
         {
-            values.Add("ControlzEx.Colors.AccentBaseColor", accentColor.ToString());
-            values.Add("ControlzEx.Colors.AccentColor80", accentColor.ToString());
-            values.Add("ControlzEx.Colors.AccentColor60", accentColor60Percent.ToString());
-            values.Add("ControlzEx.Colors.AccentColor40", accentColor40Percent.ToString());
-            values.Add("ControlzEx.Colors.AccentColor20", accentColor20Percent.ToString());
+            values.Add("ControlzEx.Colors.AccentBaseColor", colorValues.AccentColor.ToString());
+            values.Add("ControlzEx.Colors.AccentColor80", colorValues.AccentColor80.ToString());
+            values.Add("ControlzEx.Colors.AccentColor60", colorValues.AccentColor60.ToString());
+            values.Add("ControlzEx.Colors.AccentColor40", colorValues.AccentColor40.ToString());
+            values.Add("ControlzEx.Colors.AccentColor20", colorValues.AccentColor20.ToString());
 
-            values.Add("ControlzEx.Colors.HighlightColor", highlightColor.ToString());
-            values.Add("ControlzEx.Colors.IdealForegroundColor", idealForegroundColor.ToString());
+            values.Add("ControlzEx.Colors.HighlightColor", colorValues.HighlightColor.ToString());
+            values.Add("ControlzEx.Colors.IdealForegroundColor", colorValues.IdealForegroundColor.ToString());
         }
     }
 }
