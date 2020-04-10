e2aba6b6ae334206e9f46d49baee5413686c6e25
Bastian Schmidt <batzen@gmx.org>
2020-04-10 19:41:25 +0200

    Extenting ScopeGuard

---
+++ Fluent.Ribbon.Tests/Internal/ScopeGuardTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Fluent.Tests.Internal
+{
+    using Fluent.Internal;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ScopeGuardTests
+    {
+        [Test]
+        public void IsActive_Marker_Should_Change_On_Dispose()
+        {
+            var guard = new ScopeGuard();
+
+            Assert.That(guard.IsActive, Is.True);
+
+            guard.Dispose();
+
+            Assert.That(guard.IsActive, Is.False);
+
+            Assert.That(() => guard.Dispose(), Throws.Nothing);
+
+            Assert.That(guard.IsActive, Is.False);
+        }
+
+        [Test]
+        public void Actions_Should_Be_Called()
+        {
+            var entryActionCallCount = 0;
+            void EntryAction() => ++entryActionCallCount;
+
+            var disposeActionCallCount = 0;
+            void DisposeAction() => ++disposeActionCallCount;
+
+            var guard = new ScopeGuard(EntryAction, DisposeAction);
+
+            Assert.That(entryActionCallCount, Is.EqualTo(1));
+            Assert.That(disposeActionCallCount, Is.EqualTo(0));
+
+            guard.Dispose();
+
+            Assert.That(entryActionCallCount, Is.EqualTo(1));
+            Assert.That(disposeActionCallCount, Is.EqualTo(1));
+
+            guard.Dispose();
+
+            Assert.That(entryActionCallCount, Is.EqualTo(1));
+            Assert.That(disposeActionCallCount, Is.EqualTo(1));
+        }
+    }
+}
--- Fluent.Ribbon/Internal/ScopeGuard.cs
+++ Fluent.Ribbon/Internal/ScopeGuard.cs
@@ -7,6 +7,30 @@
     /// </summary>
     public class ScopeGuard : IDisposable
     {
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        private readonly Action onEntry;
+        private readonly Action onDispose;
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        public ScopeGuard()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="onEntry">Action being called on entry.</param>
+        /// <param name="onDispose">Action being called on dispose.</param>
+        public ScopeGuard(Action onEntry, Action onDispose)
+        {
+            this.onEntry = onEntry ?? throw new ArgumentNullException(nameof(onEntry));
+            this.onDispose = onDispose ?? throw new ArgumentNullException(nameof(onDispose));
+
+            this.onEntry();
+        }
+
         /// <summary>
         /// Gets whether this instance is still active (not disposed) or not.
         /// </summary>
@@ -15,7 +39,13 @@
         /// <inheritdoc />
         public void Dispose()
         {
+            var wasActive = this.IsActive;
             this.IsActive = false;
+
+            if (wasActive)
+            {
+                this.onDispose?.Invoke();
+            }
         }
     }
 }
