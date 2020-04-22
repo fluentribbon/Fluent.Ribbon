b6f32afea72fbafddfe9c382b49809129e0cd8ec
Bastian Schmidt <batzen@gmx.org>
2020-04-22 19:59:01 +0200

    Fixing data automation peer for ribbon group #647

--- Fluent.Ribbon/Automation/Peers/RibbonControlDataAutomationPeer.cs
+++ Fluent.Ribbon/Automation/Peers/RibbonControlDataAutomationPeer.cs
@@ -1,6 +1,8 @@
 ﻿namespace Fluent.Automation.Peers
 {
+    using System.Windows;
     using System.Windows.Automation.Peers;
+    using System.Windows.Controls;
 
     /// <summary>
     /// Automation peer for ribbon control items.
@@ -18,13 +20,53 @@
         /// <inheritdoc />
         protected override AutomationControlType GetAutomationControlTypeCore()
         {
-            return AutomationControlType.DataItem;
+            return AutomationControlType.ListItem;
         }
 
         /// <inheritdoc />
         protected override string GetClassNameCore()
         {
-            return "ItemsControlItem";
+            var wrapperPeer = this.GetWrapperPeer();
+            return wrapperPeer?.GetClassName() ?? string.Empty;
+        }
+
+        /// <inheritdoc />
+        public override object GetPattern(PatternInterface patternInterface)
+        {
+            // Doesnt implement any patterns of its own, so just forward to the wrapper peer. 
+            var wrapperPeer = this.GetWrapperPeer();
+
+            return wrapperPeer?.GetPattern(patternInterface);
+        }
+
+        private UIElement GetWrapper()
+        {
+            var itemsControlAutomationPeer = this.ItemsControlAutomationPeer;
+
+            var owner = (ItemsControl)itemsControlAutomationPeer?.Owner;
+            return owner?.ItemContainerGenerator.ContainerFromItem(this.Item) as UIElement;
+        }
+
+        private AutomationPeer GetWrapperPeer()
+        {
+            var wrapper = this.GetWrapper();
+            if (wrapper is null)
+            {
+                return null;
+            }
+
+            var wrapperPeer = UIElementAutomationPeer.CreatePeerForElement(wrapper);
+            if (!(wrapperPeer is null))
+            {
+                return wrapperPeer;
+            }
+
+            if (wrapper is FrameworkElement element)
+            {
+                return new FrameworkElementAutomationPeer(element);
+            }
+
+            return new UIElementAutomationPeer(wrapper);
         }
     }
 }
