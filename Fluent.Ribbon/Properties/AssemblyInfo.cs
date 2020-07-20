92e5518d091c3856eae0a0ec23f851eff3436824
Ger Hobbelt <ger@hobbelt.com>
2020-07-20 20:16:16 +0200

    fix Visual Studio error report about test project *not* having access to the internals of the fluent.Ribbon library: nuke the public key.

--- Fluent.Ribbon/Properties/AssemblyInfo.cs
+++ Fluent.Ribbon/Properties/AssemblyInfo.cs
@@ -11,5 +11,6 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Theming")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent.Metro.Behaviours")]
 
-[assembly: InternalsVisibleTo("Fluent.Ribbon.Showcase, PublicKey=002400000480000094000000060200000024000052534131000400000100010003e4bf649b00e91e7ba00628cbf54e2f97c2c9c547d8740534a57224c42e2ebdb28bac992119794a76c50f1a7f88c7af3203a4bb59501d40e10e9b6ec302d5fca6f23c0f8e800eaf5cc6f7ac682e6c50641a74b09e2e80d1905b01fd8707daacf10708faf7193233378fa909f211e77e0e7fa066e037a0c087d4b74ecf0467ae")]
-[assembly: InternalsVisibleTo("Fluent.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010003e4bf649b00e91e7ba00628cbf54e2f97c2c9c547d8740534a57224c42e2ebdb28bac992119794a76c50f1a7f88c7af3203a4bb59501d40e10e9b6ec302d5fca6f23c0f8e800eaf5cc6f7ac682e6c50641a74b09e2e80d1905b01fd8707daacf10708faf7193233378fa909f211e77e0e7fa066e037a0c087d4b74ecf0467ae")]
+// Allow internals to be accessible by the unit test code:
+[assembly: InternalsVisibleTo("Fluent.Ribbon.Showcase")]
+[assembly: InternalsVisibleTo("Fluent.Tests")]
