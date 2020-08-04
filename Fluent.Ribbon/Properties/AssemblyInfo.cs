9f5d23e9f7936c80c0b37e397e806bf03f9ed8cb
Bastian Schmidt <batzen@gmx.org>
2020-08-04 19:04:14 +0200

    Preparation for CLSCompliant

--- Fluent.Ribbon/Properties/AssemblyInfo.cs
+++ Fluent.Ribbon/Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
 [assembly: Guid("d849f751-e8f4-480d-acc0-6148eafcaafc")]
+
+[assembly: CLSCompliant(false)]
 
 [assembly: XmlnsPrefix("urn:fluent-ribbon", "fluent")]
 [assembly: XmlnsDefinition("urn:fluent-ribbon", "Fluent")]
