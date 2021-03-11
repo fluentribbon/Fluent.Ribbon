a7d3d00c6b98294785ae1f0eb2ce0be3f525b6f3
Bastian Schmidt <batzen@gmx.org>
2021-03-11 20:14:03 +0100

    Simplifying code

--- Fluent.Ribbon/Converters/StaticConverters.cs
+++ Fluent.Ribbon/Converters/StaticConverters.cs
@@ -8,26 +8,26 @@
         /// <summary>
         /// Get a static instance of <see cref="InvertNumericConverter"/>
         /// </summary>
-        public static readonly InvertNumericConverter InvertNumericConverter = new InvertNumericConverter();
+        public static readonly InvertNumericConverter InvertNumericConverter = new();
 
         /// <summary>
         /// Get a static instance of <see cref="ThicknessConverter"/>
         /// </summary>
-        public static readonly ThicknessConverter ThicknessConverter = new ThicknessConverter();
+        public static readonly ThicknessConverter ThicknessConverter = new();
 
         /// <summary>
         /// Get a static instance of <see cref="ObjectToImageConverter"/>
         /// </summary>
-        public static readonly ObjectToImageConverter ObjectToImageConverter = new ObjectToImageConverter();
+        public static readonly ObjectToImageConverter ObjectToImageConverter = new();
 
         /// <summary>
         /// Get a static instance of <see cref="ColorToSolidColorBrushValueConverter"/>
         /// </summary>
-        public static readonly ColorToSolidColorBrushValueConverter ColorToSolidColorBrushValueConverter = new ColorToSolidColorBrushValueConverter();
+        public static readonly ColorToSolidColorBrushValueConverter ColorToSolidColorBrushValueConverter = new();
 
         /// <summary>
         /// Get a static instance of <see cref="EqualsToVisibilityConverter"/>
         /// </summary>
-        public static readonly EqualsToVisibilityConverter EqualsToVisibilityConverter = new EqualsToVisibilityConverter();
+        public static readonly EqualsToVisibilityConverter EqualsToVisibilityConverter = new();
     }
 }
