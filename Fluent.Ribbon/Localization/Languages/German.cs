897399b8b13f7df0a46755779782110996bd8c20
Bastian Schmidt <batzen@gmx.org>
2021-10-20 14:17:58 +0200

    Updating German localization

--- Fluent.Ribbon/Localization/Languages/German.cs
+++ Fluent.Ribbon/Localization/Languages/German.cs
@@ -9,10 +9,10 @@ namespace Fluent.Localization.Languages
         public override string BackstageButtonKeyTip { get; } = "D";
         public override string BackstageButtonText { get; } = "Datei";
         public override string CustomizeStatusBar { get; } = "Statusleiste anpassen";
-        public override string DisplayOptionsButtonScreenTipText { get; } = FallbackLocalization.DisplayOptionsButtonScreenTipText /* Configure Ribbon display options. */;
-        public override string DisplayOptionsButtonScreenTipTitle { get; } = FallbackLocalization.DisplayOptionsButtonScreenTipTitle /* Ribbon Display Options */;
-        public override string ExpandRibbon { get; } = FallbackLocalization.ExpandRibbon /* Expand the Ribbon */;
-        public override string MinimizeRibbon { get; } = FallbackLocalization.MinimizeRibbon /* Minimize the Ribbon */;
+        public override string DisplayOptionsButtonScreenTipText { get; } = "Anzeigeoptionen für das Menüband konfigurieren.";
+        public override string DisplayOptionsButtonScreenTipTitle { get; } = "Anzeigeoptionen für das Menüband";
+        public override string ExpandRibbon { get; } = "Menüband erweitern";
+        public override string MinimizeRibbon { get; } = "Menüband minimieren";
         public override string MoreColors { get; } = "Weitere Farben...";
         public override string NoColor { get; } = "Keine Farbe";
         public override string QuickAccessToolBarDropDownButtonTooltip { get; } = "Symbolleiste für den Schnellzugriff anpassen";
@@ -30,10 +30,10 @@ namespace Fluent.Localization.Languages
         public override string RibbonContextMenuRemoveItem { get; } = "Aus Symbolleiste für den Schnellzugriff entfernen";
         public override string RibbonContextMenuShowAbove { get; } = "Symbolleiste für den Schnellzugriff über dem Menüband anzeigen";
         public override string RibbonContextMenuShowBelow { get; } = "Symbolleiste für den Schnellzugriff unter dem Menüband anzeigen";
-        public override string RibbonLayout { get; } = FallbackLocalization.RibbonLayout /* Ribbon Layout */;
+        public override string RibbonLayout { get; } = "Menüband Layout";
         public override string ScreenTipDisableReasonHeader { get; } = "Diese Funktion ist momentan deaktiviert.";
         public override string ScreenTipF1LabelHeader { get; } = "Drücken Sie F1 für die Hilfe";
-        public override string ShowRibbon { get; } = FallbackLocalization.ShowRibbon /* Show Ribbon */;
+        public override string ShowRibbon { get; } = "Menüband anzeigen";
         public override string UseClassicRibbon { get; } = "_Klassisches Menüband verwenden";
         public override string UseSimplifiedRibbon { get; } = "_Vereinfachtes Menüband verwenden";
     }
