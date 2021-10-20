8fc725b68615abac77286a4ea83ea1b09e852d39
Bastian Schmidt <batzen@gmx.org>
2021-10-20 14:59:50 +0200

    Trying to fix gitversion in PRs

--- .build/Build.cs
+++ .build/Build.cs
@@ -44,7 +44,7 @@ class Build : NukeBuild
     
     [Solution(GenerateProjects = true)] readonly Solution Solution = null!;
     
-    [GitVersion(Framework = "netcoreapp3.1")] readonly GitVersion? GitVersion;
+    [GitVersion(Framework = "netcoreapp3.1", NoFetch = true)] readonly GitVersion? GitVersion;
     
     string AssemblySemVer => GitVersion?.AssemblySemVer ?? "1.0.0";
     string SemVer => GitVersion?.SemVer ?? "1.0.0";
