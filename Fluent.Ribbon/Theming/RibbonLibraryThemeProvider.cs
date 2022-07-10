2e75a892d9849fe3ec748abb4e5b418d81deee20
Bastian Schmidt <batzen@gmx.org>
2022-07-10 19:09:11 +0200

    Fixing build

--- Fluent.Ribbon/Theming/RibbonLibraryThemeProvider.cs
+++ Fluent.Ribbon/Theming/RibbonLibraryThemeProvider.cs
@@ -22,13 +22,13 @@ public class RibbonLibraryThemeProvider : LibraryThemeProvider
     /// <inheritdoc />
     public override void FillColorSchemeValues(Dictionary<string, string> values, RuntimeThemeColorValues colorValues)
     {
-        values.Add("Fluent.Ribbon.Colors.AccentBase", colorValues.Accent.ToString());
-        values.Add("Fluent.Ribbon.Colors.Accent80", colorValues.Accent80.ToString());
-        values.Add("Fluent.Ribbon.Colors.Accent60", colorValues.Accent60.ToString());
-        values.Add("Fluent.Ribbon.Colors.Accent40", colorValues.Accent40.ToString());
-        values.Add("Fluent.Ribbon.Colors.Accent20", colorValues.Accent20.ToString());
+        values.Add("Fluent.Ribbon.Colors.AccentBase", colorValues.AccentColor.ToString());
+        values.Add("Fluent.Ribbon.Colors.Accent80", colorValues.AccentColor80.ToString());
+        values.Add("Fluent.Ribbon.Colors.Accent60", colorValues.AccentColor60.ToString());
+        values.Add("Fluent.Ribbon.Colors.Accent40", colorValues.AccentColor40.ToString());
+        values.Add("Fluent.Ribbon.Colors.Accent20", colorValues.AccentColor20.ToString());
 
-        values.Add("Fluent.Ribbon.Colors.Highlight", colorValues.Highlight.ToString());
-        values.Add("Fluent.Ribbon.Colors.IdealForeground", colorValues.IdealForeground.ToString());
+        values.Add("Fluent.Ribbon.Colors.Highlight", colorValues.HighlightColor.ToString());
+        values.Add("Fluent.Ribbon.Colors.IdealForeground", colorValues.IdealForegroundColor.ToString());
     }
 }
