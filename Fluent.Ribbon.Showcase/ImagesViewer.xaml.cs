521a9f273088bd7e7eeb611b72a7a3a183192086
Bastian Schmidt <batzen@gmx.org>
2023-04-19 11:10:55 +0200

    Fixing ImagesViewerDesignTimeData

--- Fluent.Ribbon.Showcase/ImagesViewer.xaml.cs
+++ Fluent.Ribbon.Showcase/ImagesViewer.xaml.cs
@@ -19,9 +19,9 @@ public class ImagesViewerDesignTimeData
 {
     public ImagesViewerDesignTimeData()
     {
-        var dictionary = new ResourceDictionary { Source = new Uri("pack://application:,,,/Fluent;component/Themes/Images.xaml", UriKind.Absolute) };
+        var dictionary = new ResourceDictionary { Source = new Uri("pack://application:,,,/Fluent;component/Themes/Styles.xaml", UriKind.Absolute) };
 
-        foreach (var key in dictionary.Keys.Cast<string>().OrderBy(x => x))
+        foreach (var key in dictionary.Keys.OfType<string>().OrderBy(x => x).Where(x => x.StartsWith("Fluent.Ribbon.Images.")))
         {
 #pragma warning disable CA1307
             this.Data.Add(new KeyValuePair<string, DrawingImage>(key.Replace("Fluent.Ribbon.Images.", string.Empty), (DrawingImage)dictionary[key]));
