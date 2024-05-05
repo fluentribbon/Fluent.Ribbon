3cbde79a0ef7a05855e5bf66ee120a1bed216e18
Bastian Schmidt <batzen@gmx.org>
2024-05-05 14:24:21 +0200

    Fixing triggers in BackstageTabItem

--- Fluent.Ribbon.Tests/Controls/SelectionTests.cs
+++ Fluent.Ribbon.Tests/Controls/SelectionTests.cs
@@ -17,7 +17,6 @@ public class SelectionTests
     }
 
     [Test]
-    [Explicit]
     public void TestStartScreenTabControl()
     {
         TestSelection<StartScreenTabControl, BackstageTabItem>();
