33e26848117a75accec876874780d21fd98dc735
Bastian Schmidt <batzen@gmx.org>
2024-05-17 11:12:43 +0200

    Improving tests

--- Fluent.Ribbon.Tests/Controls/RibbonTabsContainerTests.cs
+++ Fluent.Ribbon.Tests/Controls/RibbonTabsContainerTests.cs
@@ -1,6 +1,8 @@
 namespace Fluent.Tests.Controls;
 
 using System.Linq;
+using System.Windows.Controls;
+using Fluent.Tests.Helper;
 using Fluent.Tests.TestClasses;
 using NUnit.Framework;
 using Size = System.Windows.Size;
@@ -9,7 +11,7 @@ using Size = System.Windows.Size;
 public class RibbonTabsContainerTests
 {
     //private static readonly Size zeroSize = default;
-    private const double ReferenceWidth = 326;
+    private const double ReferenceWidth = 320;
 
     private const double ReferenceHeight = 25;
 
@@ -27,36 +29,38 @@ public class RibbonTabsContainerTests
     [Test]
     public void With_One_Tab()
     {
-        var container = new RibbonTabsContainer();
+        var tabsContainer = new RibbonTabsContainer();
         var tabItem = new RibbonTabItem();
-        container.Children.Add(tabItem);
+        tabsContainer.Children.Add(tabItem);
 
-        using (new TestRibbonWindow(container) { Width = ReferenceWidth })
+        var container = new ContentControl { Content = tabsContainer, Width = ReferenceWidth };
+
+        using (new TestRibbonWindow(container))
         {
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(18, ReferenceHeight)));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(18, ReferenceHeight)));
 
             tabItem.Header = "ABC";
 
-            container.UpdateLayout();
+            tabsContainer.UpdateLayout();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(42, ReferenceHeight)));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(42, ReferenceHeight)));
         }
     }
 
     [Test]
     public void With_Many_Tab()
     {
-        var container = new RibbonTabsContainer();
+        var tabsContainer = new RibbonTabsContainer();
 
         var longestTab = new RibbonTabItem { Header = "Longest header text" };
         var secondLongestTab = new RibbonTabItem { Header = "Header text" };
         var middleTab = new RibbonTabItem { Header = "Header" };
         var smallTab = new RibbonTabItem { Header = "Text" };
 
-        container.Children.Add(longestTab);
-        container.Children.Add(secondLongestTab);
-        container.Children.Add(middleTab);
-        container.Children.Add(smallTab);
+        tabsContainer.Children.Add(longestTab);
+        tabsContainer.Children.Add(secondLongestTab);
+        tabsContainer.Children.Add(middleTab);
+        tabsContainer.Children.Add(smallTab);
 
         var childrenWidths = new[]
         {
@@ -66,36 +70,26 @@ public class RibbonTabsContainerTests
             smallTab
         }.Select(x => x.DesiredSize.Width);
 
-        using (var testWindow = new TestRibbonWindow(container) { Width = ReferenceWidth })
+        var container = new ContentControl { Content = tabsContainer, Width = ReferenceWidth };
+
+        using (var testWindow = new TestRibbonWindow(container))
         {
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(310, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(container.ViewportWidth));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(317, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(tabsContainer.ViewportWidth));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
-                129,
-                83,
-                57,
-                41
+                131,
+                85,
+                59,
+                42
             }));
 
-            container.Measure(new Size(300, ReferenceHeight));
+            container.Width = 300;
+            UIHelper.DoEvents();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(300, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(container.ViewportWidth));
-
-            Assert.That(childrenWidths, Is.EquivalentTo(new[]
-            {
-                129,
-                83,
-                57,
-                41
-            }));
-
-            container.Measure(new Size(299, ReferenceHeight));
-
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(299, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(container.ViewportWidth));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(300, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(tabsContainer.ViewportWidth));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
@@ -105,10 +99,11 @@ public class RibbonTabsContainerTests
                 38
             }));
 
-            container.Measure(new Size(290, ReferenceHeight));
+            container.Width = 299;
+            UIHelper.DoEvents();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(290, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(container.ViewportWidth));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(299, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(tabsContainer.ViewportWidth));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
@@ -118,10 +113,11 @@ public class RibbonTabsContainerTests
                 38
             }));
 
-            container.Measure(new Size(289, ReferenceHeight));
+            container.Width = 290;
+            UIHelper.DoEvents();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(289, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(container.ViewportWidth));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(290, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(tabsContainer.ViewportWidth));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
@@ -131,10 +127,25 @@ public class RibbonTabsContainerTests
                 36
             }));
 
-            container.Measure(new Size(230, ReferenceHeight));
+            container.Width = 289;
+            UIHelper.DoEvents();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(230, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(285));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(289, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(tabsContainer.ViewportWidth));
+
+            Assert.That(childrenWidths, Is.EquivalentTo(new[]
+            {
+                123,
+                77,
+                53,
+                36
+            }));
+
+            container.Width = 230;
+            UIHelper.DoEvents();
+
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(230, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(285));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
@@ -144,23 +155,11 @@ public class RibbonTabsContainerTests
                 34
             }));
 
-            container.Measure(new Size(150, ReferenceHeight));
+            container.Width = 150;
+            UIHelper.DoEvents();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(150, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(285));
-
-            Assert.That(childrenWidths, Is.EquivalentTo(new[]
-            {
-                123,
-                77,
-                51,
-                34
-            }));
-
-            container.Measure(new Size(130, ReferenceHeight));
-
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(130, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(285));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(150, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(285));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
@@ -170,10 +169,25 @@ public class RibbonTabsContainerTests
                 34
             }));
 
-            container.Measure(new Size(120, ReferenceHeight));
+            container.Width = 130;
+            UIHelper.DoEvents();
 
-            Assert.That(container.DesiredSize, Is.EqualTo(new Size(120, ReferenceHeight)));
-            Assert.That(container.ExtentWidth, Is.EqualTo(282));
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(130, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(285));
+
+            Assert.That(childrenWidths, Is.EquivalentTo(new[]
+            {
+                123,
+                77,
+                51,
+                34
+            }));
+
+            container.Width = 120;
+            UIHelper.DoEvents();
+
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(120, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(282));
 
             Assert.That(childrenWidths, Is.EquivalentTo(new[]
             {
@@ -181,6 +195,31 @@ public class RibbonTabsContainerTests
                 77,
                 51,
                 34
+            }));
+
+            container.Width = 300;
+            UIHelper.DoEvents();
+
+            Assert.That(tabsContainer.DesiredSize, Is.EqualTo(new Size(300, ReferenceHeight)));
+            Assert.That(tabsContainer.ExtentWidth, Is.EqualTo(tabsContainer.ViewportWidth));
+
+            Assert.That(childrenWidths, Is.EquivalentTo(new[]
+            {
+                127,
+                81,
+                55,
+                37
+            }));
+
+            container.Width = ReferenceWidth;
+            UIHelper.DoEvents();
+
+            Assert.That(childrenWidths, Is.EquivalentTo(new[]
+            {
+                131,
+                85,
+                59,
+                42
             }));
         }
     }
