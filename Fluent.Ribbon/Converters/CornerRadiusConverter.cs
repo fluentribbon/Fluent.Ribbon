f3cad3e35fe54eee0da331f93848c4370a4c60c2
Bastian Schmidt <batzen@gmx.org>
2024-06-09 10:13:42 +0200

    Preventing binding leaks for CornerRadius

--- Fluent.Ribbon/Converters/CornerRadiusConverter.cs
+++ Fluent.Ribbon/Converters/CornerRadiusConverter.cs
@@ -8,17 +8,46 @@ using System.Windows.Data;
 /// <summary>
 /// Used to convert from four double values to <see cref="CornerRadius"/>
 /// </summary>
-public class CornerRadiusConverter : IMultiValueConverter
+[ValueConversion(typeof(CornerRadius), typeof(CornerRadius))]
+[ValueConversion(typeof(IConvertible[]), typeof(CornerRadius))]
+public class CornerRadiusConverter : IValueConverter, IMultiValueConverter
 {
+    /// <inheritdoc />
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var valuesToExtract = parameter is CornerRadiusPart cornerRadiusValue
+            ? cornerRadiusValue
+            : CornerRadiusPart.All;
+
+        var source = (CornerRadius)value!;
+
+        var topLeft = valuesToExtract.HasFlag(CornerRadiusPart.TopLeft) ? source.TopLeft : 0;
+        var topRight = valuesToExtract.HasFlag(CornerRadiusPart.TopRight) ? source.TopRight : 0;
+        var bottomRight = valuesToExtract.HasFlag(CornerRadiusPart.BottomRight) ? source.BottomRight : 0;
+        var bottomLeft = valuesToExtract.HasFlag(CornerRadiusPart.BottomLeft) ? source.BottomLeft : 0;
+
+        return new CornerRadius(topLeft, topRight, bottomRight, bottomLeft);
+    }
+
+    /// <inheritdoc />
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
     #region Implementation of IMultiValueConverter
 
     /// <inheritdoc />
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        var topLeft = TryConvertSingleValue(values[0]);
-        var topRight = TryConvertSingleValue(values[1]);
-        var bottomRight = TryConvertSingleValue(values[2]);
-        var bottomLeft = TryConvertSingleValue(values[3]);
+        var valuesToExtract = parameter is CornerRadiusPart cornerRadiusValue
+            ? cornerRadiusValue
+            : CornerRadiusPart.All;
+
+        var topLeft = valuesToExtract.HasFlag(CornerRadiusPart.TopLeft) ? TryConvertSingleValue(values[0]) : 0;
+        var topRight = valuesToExtract.HasFlag(CornerRadiusPart.TopRight) ? TryConvertSingleValue(values[1]) : 0;
+        var bottomRight = valuesToExtract.HasFlag(CornerRadiusPart.BottomRight) ? TryConvertSingleValue(values[2]) : 0;
+        var bottomLeft = valuesToExtract.HasFlag(CornerRadiusPart.BottomLeft) ? TryConvertSingleValue(values[3]) : 0;
 
         return new CornerRadius(topLeft, topRight, bottomRight, bottomLeft);
     }
@@ -42,4 +71,41 @@ public class CornerRadiusConverter : IMultiValueConverter
             return 0;
         }
     }
+}
+
+/// <summary>
+/// Defines parts of a <see cref="CornerRadius"/>.
+/// </summary>
+[Flags]
+public enum CornerRadiusPart
+{
+    /// <summary>
+    /// None.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Top left.
+    /// </summary>
+    TopLeft = 1 << 1,
+
+    /// <summary>
+    /// Top right.
+    /// </summary>
+    TopRight = 1 << 2,
+
+    /// <summary>
+    /// Bottom right.
+    /// </summary>
+    BottomRight = 1 << 3,
+
+    /// <summary>
+    /// Bottom left.
+    /// </summary>
+    BottomLeft = 1 << 4,
+
+    /// <summary>
+    /// All parts.
+    /// </summary>
+    All = TopLeft | TopRight | BottomRight | BottomLeft
 }
