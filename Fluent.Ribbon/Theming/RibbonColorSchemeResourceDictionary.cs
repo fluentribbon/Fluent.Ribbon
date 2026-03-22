4eb1389a1d8ff5db9da20c4b33861ff41293fbda
Bastian Schmidt <batzen@gmx.org>
2026-03-22 08:40:59 +0100

    Adding RibbonColorSchemeResourceDictionary

---
+++ Fluent.Ribbon/Theming/RibbonColorSchemeResourceDictionary.cs
@@ -0,0 +1,57 @@
+﻿namespace Fluent.Theming;
+
+using System.Windows;
+using System.Windows.Media;
+using ControlzEx.Theming;
+
+/// <summary>
+/// Custom resource dictionary for Ribbon runtime generated color schemes.
+/// </summary>
+public class RibbonColorSchemeResourceDictionary : ResourceDictionary
+{
+    /// <summary>
+    /// Gets or sets the options used to generate the color scheme.
+    /// </summary>
+    public RibbonColorSchemeResourceDictionaryOptions? Options
+    {
+        get;
+
+        init
+        {
+            field = value;
+
+            this.UpdateResources();
+        }
+    }
+
+    private void UpdateResources()
+    {
+        this.MergedDictionaries.Clear();
+
+        if (this.Options is { } options)
+        {
+            this.MergedDictionaries.Add(RuntimeThemeGenerator.Current.GenerateRuntimeLibraryTheme(options.BaseColor, options.PrimaryAccentColor, options.IsHighContrast, RibbonLibraryThemeProvider.DefaultInstance)!.Resources);
+        }
+    }
+}
+
+/// <summary>
+/// Options used to generate the color scheme in <see cref="RibbonColorSchemeResourceDictionary"/>.
+/// </summary>
+public class RibbonColorSchemeResourceDictionaryOptions
+{
+    /// <summary>
+    /// The base color of the color scheme.
+    /// </summary>
+    public required string BaseColor { get; init; } = null!;
+
+    /// <summary>
+    /// The primary accent color of the color scheme.
+    /// </summary>
+    public required Color PrimaryAccentColor { get; init; }
+
+    /// <summary>
+    /// Defines whether the color scheme is high contrast.
+    /// </summary>
+    public bool IsHighContrast { get; init; }
+}
