8dead6157f223578e804c08c03ddae21d7e7f67b
Bastian Schmidt <batzen@gmx.org>
2026-04-25 10:26:44 +0200

    Adding support for rounded corners on controls

---
+++ Fluent.Ribbon/Controls/ClippingBorder.cs
@@ -0,0 +1,59 @@
+﻿namespace Fluent.Controls;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+/// <summary>
+/// Draws a border, background, or both around another element and applies a clipping to the child.
+/// </summary>
+public class ClippingBorder : VisualClippingBorder
+{
+}
+
+/// <summary>
+/// Draws a border, background, or both around another element and applies a clipping to the child.
+/// </summary>
+public class VisualClippingBorder : Border
+{
+    private Border? opacityBorder;
+    private Brush? opacityMask;
+
+    /// <inheritdoc />
+    public override UIElement? Child
+    {
+        get => base.Child;
+        set
+        {
+            base.Child = value;
+
+            this.opacityBorder = this.CreateOpacityBorder();
+            this.opacityMask = new VisualBrush(this.opacityBorder);
+            value?.OpacityMask = this.opacityMask;
+        }
+    }
+
+    /// <inheritdoc />
+    protected override Size ArrangeOverride(Size finalSize)
+    {
+        // this.opacityBorder?.Width = this.ActualWidth;
+        // this.opacityBorder?.Height = this.ActualHeight;
+        this.opacityBorder?.Measure(finalSize);
+        this.opacityBorder?.Arrange(new(finalSize));
+
+        return base.ArrangeOverride(finalSize);
+    }
+
+    /// <summary>
+    /// Creates the opacity border.
+    /// </summary>
+    /// <returns></returns>
+    protected virtual Border CreateOpacityBorder()
+    {
+        return new Border
+        {
+            Background = Brushes.Magenta,
+            CornerRadius = this.CornerRadius
+        };
+    }
+}
